024495a6593e1bfca11bb01581abd4927b94c03b
Harry Liu <hliu@Harrys-MacBook-Pro.local>
2023-07-07 21:57:49 -0400

    fix: add app routing module to navbar

--- personal-website/src/app/components/navbar/navbar.component.ts
+++ personal-website/src/app/components/navbar/navbar.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
+import { AppRoutingModule } from '@app/app-routing.module';
 import { TranslateModule } from '@ngx-translate/core';
 
 @Component({
   standalone: true,
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
-  imports: [TranslateModule],
+  imports: [TranslateModule, AppRoutingModule],
 })
 export class NavbarComponent {}
