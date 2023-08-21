bef1269c50ca4aff2a627492c94f1f05a1dca6f9
Harry <harryliu1995@gmail.com>
2023-08-21 04:36:30 -0400

    feat: add demo express server

---
+++ express-demo/app.ts
@@ -0,0 +1,21 @@
+import express from "express";
+import {
+  errorLogger,
+  errorResponder,
+  invalidPathHandler,
+} from "./errormiddleware";
+import { requestLogger } from "./middleware";
+import { router } from "./router";
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(router);
+app.use(requestLogger);
+app.use(errorLogger);
+app.use(errorResponder);
+app.use(invalidPathHandler);
+
+app.listen(PORT, () => {
+  console.log(`Server listening at http://localhost:${PORT}`);
+});
---
+++ express-demo/middleware.ts
@@ -0,0 +1,12 @@
+export const requestLogger = (request, response, next) => {
+  console.log("Request Logged");
+  next();
+};
+
+export const requireJsonContent = (request, response, next) => {
+  if (request.headers["content-type"] !== "application/json") {
+    response.status(400).send("Server requires application/json");
+  } else {
+    next();
+  }
+};
