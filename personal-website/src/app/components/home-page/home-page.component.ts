859893e493fe13d6f5f8ea7b0217230e22b1d44c
Harry <harryliu1995@gmail.com>
2023-10-13 10:21:09 -0400

    feat: update home page

--- personal-website/src/app/components/home-page/home-page.component.ts
+++ personal-website/src/app/components/home-page/home-page.component.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
-import { TranslateModule } from '@ngx-translate/core';
+import { ConstructionWarningModule } from '@components/construction-warning/construction-warning.module';
 
 @Component({
   standalone: true,
   selector: 'app-home-page',
   templateUrl: './home-page.component.html',
-  imports: [TranslateModule],
+  imports: [ConstructionWarningModule],
 })
 export class HomePageComponent {}
