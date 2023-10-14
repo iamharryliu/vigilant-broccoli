6a74712b6bfdaf53f5a6f365e5cefe25fff6d0f6
Harry <harryliu1995@gmail.com>
2023-10-14 19:48:05 -0400

    feat: add links to home page cards and make links open in new tab

--- personal-website/src/app/components/home-page/home-page.component.ts
+++ personal-website/src/app/components/home-page/home-page.component.ts
@@ -1,3 +1,4 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { TranslateModule } from '@ngx-translate/core';
 
@@ -5,6 +6,17 @@ import { TranslateModule } from '@ngx-translate/core';
   standalone: true,
   selector: 'app-home-page',
   templateUrl: './home-page.component.html',
-  imports: [TranslateModule],
+  imports: [CommonModule, TranslateModule],
 })
-export class HomePageComponent {}
+export class HomePageComponent {
+  LINKS = [
+    {
+      URL: 'LINKS.OTHER.LINKEDIN.URL',
+      TEXT: 'LINKS.OTHER.LINKEDIN.TEXT',
+    },
+    {
+      URL: 'LINKS.OTHER.GITHUB.URL',
+      TEXT: 'LINKS.OTHER.GITHUB.TEXT',
+    },
+  ];
+}
