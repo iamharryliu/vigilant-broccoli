785f9ef4323af8b743cb3d5e94658fb28999d850
Harry <harryliu1995@gmail.com>
2023-10-17 10:51:30 -0400

    feat: add endpoint to personal-website-backend app to send email

--- personal-website-backend/router.ts
+++ personal-website-backend/router.ts
@@ -1,5 +1,6 @@
 import express from 'express';
 import cors from 'cors';
+import nodemailer from 'nodemailer';
 import { EmailSubscription, CORS_OPTIONS } from './app.const';
 
 export const router = express.Router();
@@ -18,6 +19,28 @@ router.post('/email-alerts', cors(CORS_OPTIONS), async (req, res) => {
     console.error(error);
     res.status(500).json({ error: 'Internal server error' });
   }
+});
+
+router.post('/send-message', cors(CORS_OPTIONS), async (req, res) => {
+  const { name, email, message } = req.body;
+  const transporter = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+      user: process.env.MY_EMAIL,
+      pass: process.env.MY_EMAIL_PASS,
+    },
+  });
+  transporter
+    .sendMail({
+      from: `'${name}' <youremail@gmail.com>`,
+      to: process.env.MY_EMAIL,
+      subject: 'Message from personal website.',
+      text: `Name: ${name}\nEmail: ${email}\nMessage: ${message}`,
+    })
+    .then(_ => {
+      return res.status(200).json({ message: 'Email sent successfully.' });
+    })
+    .catch(console.error);
 });
 
 module.exports = { router };
