7712357fb2103bdb548c3d513b5cebbcabef616e
Harry <harryliu1995@gmail.com>
2023-10-26 10:20:08 -0400

    refactor: refactor page

--- personal-website/scripts/messageSubscribers/messageSubscribers.js
+++ personal-website/scripts/messageSubscribers/messageSubscribers.js
@@ -1,35 +1,35 @@
 import { MongoClient } from 'mongodb';
 import MailService from '../../../scripts/mailService/mailService.js';
 
-const uri = `mongodb+srv://${process.env.MONGO_DB_USERNAME}:${process.env.MONGO_DB_PASSWORD}@cluster0.txzecw2.mongodb.net/`;
-const client = new MongoClient(uri);
+const URL = `mongodb+srv://${process.env.MONGO_DB_USERNAME}:${process.env.MONGO_DB_PASSWORD}@cluster0.txzecw2.mongodb.net/`;
+const client = new MongoClient(URL);
 
-const mailService = new MailService(
-  'gmail',
-  process.env.MY_EMAIL,
-  process.env.MY_EMAIL_PASSWORD,
-);
+sendNewsletter();
 
 async function sendNewsletter() {
-  sendEmails(await getEmails());
+  const emails = await getEmails();
+  sendEmails(emails);
 }
-sendNewsletter().catch(console.dir);
 
 async function getEmails() {
   try {
     const database = client.db(process.env.MONGO_DB_NAME);
     const emailSubscriptionsCollection =
       database.collection('emailsubscriptions');
-
     const emailSubscriptions = await emailSubscriptionsCollection
       .find({})
       .toArray();
-
     return emailSubscriptions.map(subscription => subscription.email);
   } finally {
     await client.close();
   }
 }
+
+const mailService = new MailService(
+  'gmail',
+  process.env.MY_EMAIL,
+  process.env.MY_EMAIL_PASSWORD,
+);
 
 function sendEmails(emails) {
   for (let email of emails) {
