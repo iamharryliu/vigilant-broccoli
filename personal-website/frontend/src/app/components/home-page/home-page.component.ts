cf0770ceab8a11a2918cfb9e5b8c06901550e4a7
Harry <harryliu1995@gmail.com>
2023-10-26 21:14:50 -0400

    feat: add vibecheck lite info card to home page

--- personal-website/frontend/src/app/components/home-page/home-page.component.ts
+++ personal-website/frontend/src/app/components/home-page/home-page.component.ts
@@ -2,12 +2,13 @@ import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { TranslateModule } from '@ngx-translate/core';
 import { ContactModule } from '@components/contact/contact.module';
+import { RouterModule } from '@angular/router';
 
 @Component({
   standalone: true,
   selector: 'app-home-page',
   templateUrl: './home-page.component.html',
-  imports: [CommonModule, TranslateModule, ContactModule],
+  imports: [CommonModule, TranslateModule, ContactModule, RouterModule],
 })
 export class HomePageComponent {
   LINKS = [
