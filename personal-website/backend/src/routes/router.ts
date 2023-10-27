23a411497f5fea87265f8aee3f4f9a77d1b45cf3
Harry <harryliu1995@gmail.com>
2023-10-26 20:22:13 -0400

    feat: add vibecheck-lite endpoint

--- personal-website/backend/src/routes/router.ts
+++ personal-website/backend/src/routes/router.ts
@@ -1,9 +1,19 @@
 import express from 'express';
 import cors from 'cors';
 import nodemailer from 'nodemailer';
+import OpenAI from 'openai';
+
 import { EmailSubscription, CORS_OPTIONS } from '../configs/app.const';
 import { requireJsonContent } from '../middlewares/middleware';
 import { logger } from '../middlewares/loggers';
+
+const OPENWEATHER_API_KEY = process.env.OPENWEATHER_API_KEY;
+const OPENAI_API_KEY = process.env.OPENAI_API_KEY;
+
+const openai = new OpenAI({
+  apiKey: OPENAI_API_KEY,
+  dangerouslyAllowBrowser: true,
+});
 
 export const router = express.Router();
 router.use(express.json({ limit: 5000 }));
@@ -72,6 +82,49 @@ router.post(
       .catch(error => {
         logger.error(error);
       });
+  },
+);
+
+async function getWeatherDataForOutfitRecommendation(coords) {
+  const { LAT, LON } = coords;
+  try {
+    const res = await fetch(
+      `https://api.openweathermap.org/data/2.5/forecast?lat=${LAT}&lon=${LON}&appid=${OPENWEATHER_API_KEY}`,
+    );
+    const data = await res.json();
+    const requestData = data.list.slice(0, 4);
+    return requestData;
+  } catch (err) {
+    console.log(err);
+  }
+}
+
+async function getOutfitRecommendation(coords) {
+  let recommendation = '';
+  try {
+    const requestData = await getWeatherDataForOutfitRecommendation(coords);
+    const requestString = `Can you recommend what to wear today with the following json data. ${JSON.stringify(
+      requestData,
+    )}. Convert and display temperature in celsius instead of Kelvin for the answer.`;
+    const completion = await openai.chat.completions.create({
+      messages: [{ role: 'system', content: requestString }],
+      model: 'gpt-3.5-turbo',
+    });
+    recommendation = completion.choices[0].message.content;
+    return recommendation;
+  } catch (err) {
+    console.log(err);
+  }
+}
+
+router.get(
+  '/get-outfit-recommendation',
+  cors(CORS_OPTIONS),
+  async (req, res) => {
+    const LAT = req.query.lat;
+    const LON = req.query.lon;
+    const recommendation = await getOutfitRecommendation({ LAT, LON });
+    res.status(200).json({ success: true, data: recommendation });
   },
 );
 
