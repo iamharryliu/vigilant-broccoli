2f49fcd9d5242e7a16e96ecef495073478eeb9e0
Harry <harryliu1995@gmail.com>
2023-11-07 14:29:47 -0500

    update(personal-website-frontend): update environment variables

--- personal-website/frontend/src/environments/environment.development.ts
+++ personal-website/frontend/src/environments/environment.development.ts
@@ -1,6 +1,14 @@
 export const ENVIRONMENT = {
   PRODUCTION: false,
-  PERSONAL_WEBSITE_FRONTEND_URL: 'http://localhost:4200',
-  PERSONAL_WEBSITE_BACKEND_URL: 'http://localhost:3000',
   RECAPTCHA_V3_SITE_KEY: '6LfAdbMoAAAAAOR8IRGqw-3gPj8Fdl5GHqm6wzOF',
+  URLS: {
+    PERSONAL_WEBSITE_FRONTEND_URL: 'http://localhost:4200',
+    PERSONAL_WEBSITE_BACKEND_URL: 'http://localhost:3000',
+    LINKEDIN: 'https://www.linkedin.com/in/iamharryliu/',
+    GITHUB: 'https://github.com/iamharryliu',
+    PERSONAL_IG: 'https://www.instagram.com/prettydamntired/',
+    SECONDHAND_STORE_IG: 'https://www.instagram.com/harrysellsshit/',
+    SKATE_IG: 'https://www.instagram.com/torontocityskate/',
+    EMAIL: 'mailto: harryliu1995@gmail.com',
+  },
 };
--- personal-website/frontend/src/environments/environment.ts
+++ personal-website/frontend/src/environments/environment.ts
@@ -1,6 +1,14 @@
 export const ENVIRONMENT = {
   PRODUCTION: true,
-  PERSONAL_WEBSITE_FRONTEND_URL: 'https://harryliu.design',
-  PERSONAL_WEBSITE_BACKEND_URL: 'https://old-wind-7127.fly.dev',
   RECAPTCHA_V3_SITE_KEY: '6LfAdbMoAAAAAOR8IRGqw-3gPj8Fdl5GHqm6wzOF',
+  URLS: {
+    PERSONAL_WEBSITE_FRONTEND_URL: 'https://harryliu.design',
+    PERSONAL_WEBSITE_BACKEND_URL: 'https://old-wind-7127.fly.dev',
+    LINKEDIN: 'https://www.linkedin.com/in/iamharryliu/',
+    GITHUB: 'https://github.com/iamharryliu',
+    PERSONAL_IG: 'https://www.instagram.com/prettydamntired/',
+    SECONDHAND_STORE_IG: 'https://www.instagram.com/harrysellsshit/',
+    SKATE_IG: 'https://www.instagram.com/torontocityskate/',
+    EMAIL: 'mailto: harryliu1995@gmail.com',
+  },
 };
