eb4a34dbda9cedffe2fa2c1535eeb95a7d51b95c
Harry <harryliu1995@gmail.com>
2023-11-07 14:31:41 -0500

    feat(personal-website-frontend): add link tree

--- personal-website/frontend/src/app/components/pages/link-tree-page/link-tree-page.component.ts
+++ personal-website/frontend/src/app/components/pages/link-tree-page/link-tree-page.component.ts
@@ -1,10 +1,29 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { LINKS } from '@app/app-route.const';
+import { CardComponent } from '@app/components/card/card.component';
+import { ButtonLinkComponent } from '@app/components/global/button-link/button-link.component';
 import { CenteredAppLayoutComponent } from '@app/components/layouts/centered-app-layout/centered-app-layout.compoenent';
 
 @Component({
   standalone: true,
   selector: 'app-link-tree-page',
   templateUrl: './link-tree-page.component.html',
-  imports: [CenteredAppLayoutComponent],
+  imports: [
+    CenteredAppLayoutComponent,
+    ButtonLinkComponent,
+    CommonModule,
+    CardComponent,
+  ],
 })
-export class LinkTreePageComponent {}
+export class LinkTreePageComponent {
+  LINKS = [
+    LINKS.PERSONAL_WEBSITE,
+    LINKS.LINKEDIN,
+    LINKS.GITHUB,
+    LINKS.PERSONAL_INSTAGRAM,
+    LINKS.SECONDHAND_STORE_IG,
+    LINKS.SKATE_IG,
+    LINKS.VIBECHECK_LITE,
+  ];
+}
