819b1478057685f673d27a000512e9ab92771af8
Harry <harryliu1995@gmail.com>
2023-11-13 11:27:46 -0500

    refactor(personal-website-frontend): undo unnecessary code

--- personal-website/frontend/src/app/core/services/app.service.ts
+++ personal-website/frontend/src/app/core/services/app.service.ts
@@ -1,15 +1,11 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 import { Injectable } from '@angular/core';
-import { TranslateService } from '@ngx-translate/core';
 
 @Injectable({
   providedIn: 'root',
 })
 export class AppService {
-  constructor(private translateService: TranslateService) {}
   init() {
-    return () => {
-      return this.translateService.use('en');
-    };
+    // App init code
   }
 }
