ba3cf32fdeb9e066a8e57c392c933810c7d70be4
Harry <harryliu1995@gmail.com>
2023-11-13 11:58:43 -0500

    update(vibecheck-lite): refactor and update script

--- node-scripts/vibecheck/vibecheck-lite/vibecheck-lite-mail.js
+++ node-scripts/vibecheck/vibecheck-lite/vibecheck-lite-mail.js
@@ -17,43 +17,48 @@ const mailService = new MailService(
   process.env.MY_EMAIL_PASSWORD,
 );
 
+class VibecheckLite {
+  static async getOutfitRecommendation() {
+    let result = '';
+    try {
+      const requestData =
+        await VibecheckLite.getWeatherDataForOutfitRecommendation();
+      const requestString = `Can you recommend what to wear today with the following json data. Please use the "dt_txt" parameter, it is in GMT, please convert it to EST for display. Please use the "temp" parameter, it is in degrees Kelvin (K), please convert it to degrees Celsius (C).${JSON.stringify(
+        requestData,
+      )}. Convert the date and time from GMT to EST for the answer. Convert the temperature to celsius instead of Kelvin for the answer. Reply using the following template: \Date {h:mm EST}:\n- Temperature: {temperature in celsius}\n- Weather: {weather}\n- Recommendation: {recommended outfit}.`;
+      const completion = await openai.chat.completions.create({
+        messages: [{ role: 'system', content: requestString }],
+        model: 'gpt-3.5-turbo',
+      });
+      result =
+        completion.choices[0].message.content +
+        `\n\n ${JSON.stringify(requestData, null, 2)}`;
+      return result;
+    } catch (err) {
+      console.log(err);
+    }
+  }
+
+  static async getWeatherDataForOutfitRecommendation() {
+    try {
+      const res = await fetch(
+        `https://api.openweathermap.org/data/2.5/forecast?q=toronto&appid=${OPENWEATHER_API_KEY}`,
+      );
+      const data = await res.json();
+      const requestData = data.list.slice(0, 3);
+      return requestData;
+    } catch (err) {
+      console.log(err);
+    }
+  }
+}
+
 main();
 
 async function main() {
   for (let email of emails) {
     const subject = `Vibecheck Lite`;
-    const message = await getOutfitRecommendation();
+    const message = await VibecheckLite.getOutfitRecommendation();
     mailService.sendEmail(email, subject, message);
-  }
-}
-
-async function getOutfitRecommendation() {
-  let recommendation = '';
-  try {
-    const requestData = await getWeatherDataForOutfitRecommendation();
-    const requestString = `Can you recommend what to wear today with the following json data. Please use the "dt_txt" parameter, it is in GMT, please convert it to EST for display. Please use the "temp" parameter, it is in degrees Kelvin (K), please convert it to degrees Celsius (C).${JSON.stringify(
-      requestData,
-    )}. Convert the date and time from GMT to EST for the answer. Convert the temperature to celsius instead of Kelvin for the answer. Reply using the following template: \nFor {time and date in EST}:\n- Temperature: {temperature in celsius}\n- Weather: {weather}\n- Recommendation: {recommended outfit}.`;
-    const completion = await openai.chat.completions.create({
-      messages: [{ role: 'system', content: requestString }],
-      model: 'gpt-3.5-turbo',
-    });
-    recommendation = completion.choices[0].message.content;
-    return recommendation;
-  } catch (err) {
-    console.log(err);
-  }
-}
-
-async function getWeatherDataForOutfitRecommendation() {
-  try {
-    const res = await fetch(
-      `https://api.openweathermap.org/data/2.5/forecast?q=toronto&appid=${OPENWEATHER_API_KEY}`,
-    );
-    const data = await res.json();
-    const requestData = data.list.slice(0, 3);
-    return requestData;
-  } catch (err) {
-    console.log(err);
   }
 }
