1466a16ed0280b6f21bbc8402eec4e37fccadb3d
Harry Liu <harryliu1995@gmail.com>
2023-11-27 12:46:35 -0500

    refactor(node-tools): move encryption service

--- node/tools/src/index.ts
+++ node/tools/src/index.ts
@@ -1,3 +1,4 @@
 export { MailService } from './mail-service/mail.service';
+export { EncryptionService } from './encryption/encryption.service';
 export * from './mail-service/mail.model';
 export * from './mongo-db/mongo-db';
