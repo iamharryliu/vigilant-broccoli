d41cecd68673829fc8ad30941449ab637a7cd292
Harry Liu <harryliu1995@gmail.com>
2023-11-28 12:00:29 -0500

    refactor(node-tools): remove useNewUrlParser option

--- node/tools/src/mongo-db/mongo-db.ts
+++ node/tools/src/mongo-db/mongo-db.ts
@@ -3,6 +3,5 @@ import { ConnectOptions } from 'mongoose';
 export const MONGO_DB_SERVER = `mongodb+srv://${process.env.MONGO_DB_USERNAME}:${process.env.MONGO_DB_PASSWORD}@cluster0.txzecw2.mongodb.net/`;
 
 export const MONGO_DB_SETTINGS = {
-  useNewUrlParser: true,
   useUnifiedTopology: true,
 } as ConnectOptions;
