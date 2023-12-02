34e354ddc65df4fc2da3d923a6b715ac434ea5d6
Harry Liu <harryliu1995@gmail.com>
2023-12-01 19:42:07 -0500

    refactor(vibecheck-lite): refactor script

--- vibecheck/scripts/vibecheck-lite-recommendation.ts
+++ vibecheck/scripts/vibecheck-lite-recommendation.ts
@@ -1,36 +1,30 @@
-import mongoose from 'mongoose';
 import {
-  MONGO_DB_SERVER,
   MailService,
   DEFAULT_EMAIL_REQUEST,
   EncryptionService,
+  MONGO_DB_CLIENT,
 } from '../../node/tools/src';
 import {
+  PERSONAL_WEBSITE_DB_COLLECTIONS,
   PERSONAL_WEBSITE_DB_DATABASES,
   PERSONAL_WEBSITE_URL,
 } from '../../personal-website/common/src';
 import { VibecheckLite } from '@prettydamntired/vibecheck-lite';
-import { EmailSubscription } from '@prettydamntired/personal-website-types';
-
-mongoose.connect(`${MONGO_DB_SERVER}/${PERSONAL_WEBSITE_DB_DATABASES.PROD}`);
-const db = mongoose.connection;
-db.getClient;
-db.on('error', error => {
-  console.error(`MongoDB connection error: ${error}`);
-});
-db.once('open', () => {
-  console.info('Connected to MongoDB');
-});
 
 main();
 
 async function main() {
   console.log('Vibecheck lite recommendation script start.');
+  const database = MONGO_DB_CLIENT.db(PERSONAL_WEBSITE_DB_DATABASES.PROD);
+  const emailSubscriptionsCollection = database.collection(
+    PERSONAL_WEBSITE_DB_COLLECTIONS.EMAIL_SUBSCRIPTIONS,
+  );
+  const subscriptionQuery = {
+    isVerified: { $eq: true },
+    vibecheckLiteSubscription: { $exists: true, $ne: null },
+  };
   const emailSubscriptions = (
-    await EmailSubscription.find({
-      isVerified: { $eq: true },
-      vibecheckLiteSubscription: { $exists: true, $ne: null },
-    })
+    await emailSubscriptionsCollection.find(subscriptionQuery).toArray()
   ).map(data => {
     return {
       email: data.email,
@@ -38,7 +32,6 @@ async function main() {
       longitude: data.vibecheckLiteSubscription?.longitude,
     };
   });
-  db.close();
 
   const emailPromises = emailSubscriptions.map(async emailSubscription => {
     const { email, latitude, longitude } = emailSubscription;
@@ -55,7 +48,7 @@ async function main() {
     })) as string;
     const token = EncryptionService.encryptData(email);
     const template = {
-      path: `${__dirname}/vibecheck-lite.ejs`,
+      path: `${__dirname}/assets/vibecheck-lite.ejs`,
       data: {
         recommendation: recommendation,
         url: `${PERSONAL_WEBSITE_URL.FRONTEND_REDIRECTED}/unsubscribe-vibecheck-lite?token=${token}`,
