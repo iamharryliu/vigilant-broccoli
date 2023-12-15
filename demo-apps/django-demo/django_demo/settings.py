de5e35bb563e8c43eb2ae18651a45b51be2566f5
Harry Liu <harryliu1995@gmail.com>
2023-12-15 13:51:04 +0100

    update(django-demo): environment secret key

--- demo-apps/django-demo/django_demo/settings.py
+++ demo-apps/django-demo/django_demo/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -20,7 +21,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-zej@0-k9vj2!cg(5=7#u(g@q$xia1s7g3#6jn6rf!(&1p!i6yd"
+SECRET_KEY = os.environ.get("SECRET_KEY")
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
