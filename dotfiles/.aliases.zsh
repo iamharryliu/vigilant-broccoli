d73e9354d48e67b402eb2e3109f8afaa60ced6c2
Harry Liu <harryliu1995@gmail.com>
2023-12-19 15:47:57 +0100

    feat(dotfiles): start dot files

---
+++ dotfiles/.aliases.zsh
@@ -0,0 +1,19 @@
+# ls (mac)
+alias ls='ls -FGhl'
+alias la='ls -A'
+alias lS='la -S'
+alias lSr='la -Sr'
+alias lt='la -t'
+alias ltr='la -rt'
+
+# Python
+alias python=python3
+alias newvenv='rm -r venv; python -m venv venv; source venv/bin/activate'
+alias makevenv='rm -r venv; python -m venv venv; source venv/bin/activate; pip install -r requirements.txt; pip freeze  > requirements.txt'
+alias runvenv='source venv/bin/activate; python run.py'
+alias venvon='source venv/bin/activate'
+
+# Git
+alias gc='git commit'
+alias gco='git checkout'
+alias ga='git add'
---
+++ dotfiles/.zshrc
@@ -0,0 +1,5 @@
+
+source ~/vigilant-broccoli/dotfiles/.aliases.zsh
+source ~/.env.sh
+
+export PATH="/opt/homebrew/bin:$PATH"
