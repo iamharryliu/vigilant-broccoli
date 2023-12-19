7547469fe6caaec5cbb11e8eaa1c8cb054af8eaa
Harry Liu <harryliu1995@gmail.com>
2023-12-19 15:46:32 +0100

    update(grind-75): unit test to do

---
+++ grind-75/test/test_construct_BT_from_preorder_and_in_order_traversal.py
@@ -0,0 +1,19 @@
+import unittest
+
+# from tree.common import BinaryTree
+# from tree.construct_BT_from_preorder_and_in_order_traversal import Solution
+
+
+# TODO
+class TestSolution(unittest.TestCase):
+    def test(self):
+        pass
+        # res = Solution.buildTree(preorder = [3,9,20,15,7], inorder = [9,3,15,20,7])
+        # res = BinaryTree.to_bfs_traversal_array(res)
+        # expected = [3,9,20,None,None,15,7]
+        # self.assertEqual(res, expected)
+
+        # res = Solution.buildTree(preorder = [-1], inorder = [-1])
+        # res = BinaryTree.to_bfs_traversal_array(res)
+        # expected = [-1]
+        # self.assertEqual(res, expected)
--- grind-75/tree/construct_BT_from_preorder_and_in_order_traversal.py
+++ grind-75/tree/construct_BT_from_preorder_and_in_order_traversal.py
@@ -1,14 +1,9 @@
 from typing import Optional, List
-
-# Definition for a binary tree node.
-class TreeNode:
-    def __init__(self, val=0, left=None, right=None):
-        self.val = val
-        self.left = left
-        self.right = right
+from tree.common import TreeNode
 
 
 class Solution:
+    @classmethod
     def buildTree(self, preorder: List[int], inorder: List[int]) -> Optional[TreeNode]:
         if not preorder or not inorder:
             return None
