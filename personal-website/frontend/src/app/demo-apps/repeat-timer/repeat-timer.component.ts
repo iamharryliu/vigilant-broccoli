75423d97392a752188d1b5a0e423002a2504312c
Harry Liu <harryliu1995@gmail.com>
2024-01-06 15:35:55 +0100

    feat(repeat timer): enhance repeat timer

--- personal-website/frontend/src/app/demo-apps/repeat-timer/repeat-timer.component.ts
+++ personal-website/frontend/src/app/demo-apps/repeat-timer/repeat-timer.component.ts
@@ -1,34 +1,44 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, OnDestroy } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { CenteredAppLayoutComponent } from '@app/components/layouts/centered-app-layout/centered-app-layout.compoenent';
 
 @Component({
-  selector: 'app-repeat-timer',
   standalone: true,
+  selector: 'app-repeat-timer',
   templateUrl: './repeat-timer.component.html',
-  imports: [CenteredAppLayoutComponent],
+  imports: [CommonModule, FormsModule, CenteredAppLayoutComponent],
 })
-export class RepeatTimerComponent {
-  playSoundAtIntervals() {
-    this.playSound();
-    const timedIntervalInMinutes = 20;
-    const now = new Date();
-    const minutesUntilNextInterval =
-      timedIntervalInMinutes - (now.getMinutes() % timedIntervalInMinutes);
-    const initialDelayInMilliseconds = minutesUntilNextInterval * 60 * 1000;
+export class RepeatTimerComponent implements OnDestroy {
+  interval: number = 1000; // Default interval in milliseconds
+  timeLeft: number = 0;
+  private timer: any;
+  isTimerRunning: boolean = false;
 
-    setTimeout(() => {
-      setInterval(
-        () => {
-          this.playSound();
-        },
-        timedIntervalInMinutes * 60 * 1000,
-      );
-    }, initialDelayInMilliseconds);
+  startTimer() {
+    this.timeLeft = this.interval / 1000;
+    this.isTimerRunning = true;
+    this.timer = setInterval(() => {
+      this.timeLeft--;
+      if (this.timeLeft === 0) {
+        this.playSound();
+        this.timeLeft = this.interval / 1000;
+      }
+    }, 1000);
+  }
+
+  stopTimer() {
+    clearInterval(this.timer);
+    this.isTimerRunning = false;
   }
 
   playSound() {
     const audio = new Audio('assets/chime.mp3');
     audio.play();
+  }
+
+  ngOnDestroy() {
+    this.stopTimer();
   }
 }
