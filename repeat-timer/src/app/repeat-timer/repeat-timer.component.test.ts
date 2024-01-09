c5c0f9bc33648262e3ece04c72f5c5abd362fd9f
Harry Liu <harryliu1995@gmail.com>
2024-01-09 09:05:52 +0100

    feat(repeat-timer): enhance repeat timer

--- repeat-timer/src/app/repeat-timer/repeat-timer.component.test.ts
+++ repeat-timer/src/app/repeat-timer/repeat-timer.component.test.ts
@@ -1,5 +1,5 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-import { RepeatTimerComponent } from './repeat-timer.component';
+import { ALARM_INTERVAL, RepeatTimerComponent } from './repeat-timer.component';
 
 describe('RepeatTimerComponent', () => {
   let component: RepeatTimerComponent;
@@ -20,40 +20,82 @@ describe('RepeatTimerComponent', () => {
     expect(component).toBeTruthy();
   });
 
-  it('should start the timer', () => {
-    const udpdateCountdownSpy = jest.spyOn(component, 'udpdateCountdown');
-    component.addMinutes(5);
-    component.startTimer();
-    expect(component.isTimerRunning).toBe(true);
-    expect(component.timer).toBeDefined();
-    expect(udpdateCountdownSpy).toHaveBeenCalled();
+  describe('addMinutes', () => {
+    it('should set internal and call setTimeLeft', () => {
+      jest.spyOn(component, 'setTimeLeft');
+      const minutes = 5;
+      component.addMinutes(minutes);
+      expect(component.interval).toEqual(minutes * 60 * 1000);
+      expect(component.setTimeLeft).toHaveBeenCalled();
+    });
   });
 
-  it('should stop the timer', () => {
-    const clearIntervalSpy = jest.spyOn(window, 'clearInterval');
-    component.stopTimer();
-    expect(clearIntervalSpy).toHaveBeenCalledWith(component.timer);
-    expect(component.isTimerRunning).toBe(false);
+  describe('startTimer', () => {
+    it('should set timer to running to true', () => {
+      component.startTimer();
+      expect(component.isTimerRunning).toEqual(true);
+    });
+
+    it('should start the timer and call updateCountdown every second', () => {
+      jest.spyOn(component, 'udpdateCountdown');
+      component.startTimer();
+      expect(component.timer).toBeDefined();
+      jest.advanceTimersByTime(1000);
+      expect(component.udpdateCountdown).toHaveBeenCalled();
+    });
+
+    it('should startAlarm when timer reaches 0', () => {
+      const testInterval = 5000;
+      jest.spyOn(component, 'startAlarm');
+      component.interval = testInterval;
+      component.setTimeLeft();
+      component.startTimer();
+      jest.advanceTimersByTime(testInterval);
+      expect(component.startAlarm).toHaveBeenCalled();
+    });
   });
 
-  it('should reset the timer', () => {
-    jest.spyOn(component, 'stopTimer');
-    jest.spyOn(component, 'setTimeleft');
-    jest.spyOn(component, 'udpdateCountdown');
-    component.addMinutes(10);
-    component.resetTimer();
-    expect(component.stopTimer).toHaveBeenCalled();
-    expect(component.interval).toBe(0);
-    expect(component.setTimeleft).toHaveBeenCalled();
-    expect(component.udpdateCountdown).toHaveBeenCalled();
+  describe('startAlarm', () => {
+    it('should call stop timer and setTimeLeft', () => {
+      jest.spyOn(component, 'stopTimer');
+      jest.spyOn(component, 'startTimer');
+      component.startAlarm();
+    });
+
+    it('should set an alarm and play sound every 3 seconds', () => {
+      jest.spyOn(component, 'playSound');
+      component.startAlarm();
+      expect(component.playSound).toHaveBeenCalled();
+      jest.advanceTimersByTime(ALARM_INTERVAL);
+      expect(component.playSound).toHaveBeenCalledTimes(2);
+    });
   });
 
-  it('should play sound when timer reaches 0', () => {
-    jest.spyOn(component, 'playSound');
-    component.interval = 5000;
-    component.setTimeleft();
-    component.startTimer();
-    jest.advanceTimersByTime(5000);
-    expect(component.playSound).toHaveBeenCalled();
+  describe('stopTimer', () => {
+    it('should clear the timer and set isTimerRunning to false', () => {
+      jest.spyOn(window, 'clearInterval');
+      component.stopTimer();
+      expect(window.clearInterval).toHaveBeenCalledWith(component.timer);
+      expect(component.isTimerRunning).toBe(false);
+    });
+  });
+
+  describe('resetTimer', () => {
+    it('should reset the timer', () => {
+      jest.spyOn(component, 'stopTimer');
+      jest.spyOn(component, 'setTimeLeft');
+      component.resetTimer();
+      expect(component.stopTimer).toHaveBeenCalled();
+      expect(component.interval).toBe(0);
+      expect(component.setTimeLeft).toHaveBeenCalled();
+    });
+  });
+
+  describe('onMouseMove', () => {
+    it('should clear the alarm when mousemove event occurs', () => {
+      jest.spyOn(window, 'clearInterval');
+      component.onMouseMove();
+      expect(window.clearInterval).toHaveBeenCalledWith(component.alarm);
+    });
   });
 });
