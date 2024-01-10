764e55229b155d8bb04b4ef6b6cd5cfc6aa29ef5
Harry Liu <harryliu1995@gmail.com>
2024-01-11 00:30:43 +0100

    feat(personal-website): add cypress e2e testing

---
+++ projects/personal-website/scripts/cypress/e2e/test.cy.js
@@ -0,0 +1,23 @@
+/// <reference types="cypress" />
+
+describe('harryliu.design', ()=>{
+  beforeEach(() => {
+    cy.visit('https://harryliu.design/')
+  })
+
+  describe('contact-form', () => {
+    it('displays two todo items by default', () => {
+      cy.get('#contact-form-name-input').type('username')
+      cy.get('#contact-form-email-input').type('user@email.com')
+      cy.get('#contact-form-message-input').type('message')
+      cy.get('#contact-form-submit-button').click()
+    })
+  });
+
+  describe('subscribe-form', () => {
+    it('displays two todo items by default', () => {
+      cy.get('#subscribe-form-email-input').type('user@email.com')
+      cy.get('#subscribe-form-submit-button').click()
+    })
+  });
+})
