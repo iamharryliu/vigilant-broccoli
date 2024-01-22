f0b333115346f0ac1140e1264620f1234509fc9b
Harry Liu <harryliu1995@gmail.com>
2024-01-22 22:33:53 +0100

    feat: new test package

---
+++ projects/libraries/npm/common/src/index.ts
@@ -0,0 +1,18 @@
+// Http
+export * from './common/http.const';
+
+// Location
+export * from './location/location.model';
+export * from './location/location.service';
+
+// Personal Website
+
+// Endpoints
+export * from './personal-website/endpoints.const';
+
+// Database
+export * from './personal-website/database/database.const';
+export * from './personal-website/database/database.model';
+
+// Logger
+export { winstonLogger as logger } from './node/winston-logger.service';
---
+++ projects/libraries/npm/common/src/location/location.service.ts
@@ -0,0 +1,16 @@
+import { Observable } from 'rxjs';
+import { Location } from './location.model';
+
+export class LocationService {
+  getLocation(): Observable<Location> {
+    return new Observable(observer => {
+      window.navigator.geolocation.getCurrentPosition(position => {
+        observer.next({
+          latitude: position.coords.latitude,
+          longitude: position.coords.longitude,
+        });
+        observer.complete();
+      });
+    });
+  }
+}
