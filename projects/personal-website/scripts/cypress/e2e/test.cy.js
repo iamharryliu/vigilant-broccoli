c0f51566b591fd306710565e2dc4fc7074a1677c
Harry Liu <harryliu1995@gmail.com>
2024-01-26 03:02:39 +0100

    test(personal-website): update e2e test

--- projects/personal-website/scripts/cypress/e2e/test.cy.js
+++ projects/personal-website/scripts/cypress/e2e/test.cy.js
@@ -1,23 +1,34 @@
 /// <reference types="cypress" />
+const url = 'https://harryliu.design/'
 
 describe('harryliu.design', () => {
   beforeEach(() => {
-    cy.visit('https://harryliu.design/');
+    cy.visit(url);
   });
 
   describe('contact-form', () => {
-    it('displays two todo items by default', () => {
+    it('should return successful response on valid input', () => {
+      cy.intercept('POST', '*', { times: 1 }).as('post')
       cy.get('#contact-form-name-input').type('username');
       cy.get('#contact-form-email-input').type('user@email.com');
       cy.get('#contact-form-message-input').type('message');
       cy.get('#contact-form-submit-button').click();
+      cy.wait('@post')
+      .then(interception => {
+        expect(interception.response.statusCode).to.eq(200)
+      })
     });
   });
 
   describe('subscribe-form', () => {
-    it('displays two todo items by default', () => {
+    it('should return successful response on valid input', () => {
+      cy.intercept('POST', '*', { times: 1 }).as('post')
       cy.get('#subscribe-form-email-input').type('user@email.com');
       cy.get('#subscribe-form-submit-button').click();
+      cy.wait('@post')
+      .then(interception => {
+        expect(interception.response.statusCode).to.eq(200)
+      })
     });
   });
 });
