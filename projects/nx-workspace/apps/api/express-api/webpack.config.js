482ccf61408fdc284ba6111bdf606b4936eb15fc
Harry Liu <harryliu1995@gmail.com>
2024-01-29 23:14:30 +0100

    style: format

--- projects/nx-workspace/apps/api/express-api/webpack.config.js
+++ projects/nx-workspace/apps/api/express-api/webpack.config.js
@@ -5,9 +5,9 @@ module.exports = composePlugins(
   withNx({
     target: 'node',
   }),
-  (config) => {
+  config => {
     // Update the webpack config as needed here.
     // e.g. `config.plugins.push(new MyPlugin())`
     return config;
-  }
+  },
 );
