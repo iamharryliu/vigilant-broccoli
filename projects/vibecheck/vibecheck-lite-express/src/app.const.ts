7a435640f38568f757252e83c1c2b9a5a703eda3
Harry Liu <harryliu1995@gmail.com>
2024-01-29 12:31:20 +0100

    refactor: rm old vibecheck-lite-express folder

--- projects/vibecheck/vibecheck-lite-express/src/app.const.ts
+++
@@ -1,5 +0,0 @@
-export const LOCAL_HOST_IP_ADDRESS = '127.0.0.1';
-export const HOST = process.env.HOST || LOCAL_HOST_IP_ADDRESS;
-export const IS_DEV_ENV = HOST === LOCAL_HOST_IP_ADDRESS;
-export const PORT = process.env.PORT || 3000;
-export const CORS_OPTIONS = { origin: true, credentials: true };
