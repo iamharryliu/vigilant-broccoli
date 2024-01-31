4054c2866009f53365ee9ee5ff307c72b38cc26b
Harry Liu <harryliu1995@gmail.com>
2024-01-31 13:25:58 +0100

    refactor: rm test nx repos

--- projects/nx-workspace/apps/api/express-api-e2e/src/express-api/express-api.spec.ts
+++
@@ -1,9 +0,0 @@
-import axios from 'axios';
-
-describe('GET /', () => {
-  it('should return a message', async () => {
-    const res = await axios.get('http://localhost:3000/api');
-
-    expect(res.status).toBe(200);
-  });
-});
