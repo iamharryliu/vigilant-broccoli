54568484d54b7b83e9dd78b437ca7e38a518cdc8
Harry Liu <harryliu1995@gmail.com>
2024-01-31 13:40:22 +0100

    refactor: folder structure

--- projects/nx-workspace/@prettydamntired/test-lib/jest.config.ts
+++ projects/nx-workspace/@prettydamntired/test-lib/jest.config.ts
@@ -1,11 +1,11 @@
 /* eslint-disable */
 export default {
   displayName: 'test-lib',
-  preset: '../../../jest.preset.js',
+  preset: '../../jest.preset.js',
   testEnvironment: 'node',
   transform: {
     '^.+\\.[tj]s$': ['ts-jest', { tsconfig: '<rootDir>/tsconfig.spec.json' }],
   },
   moduleFileExtensions: ['ts', 'js', 'html'],
-  coverageDirectory: '../../../coverage/libs/shared/test-lib',
+  coverageDirectory: '../../coverage/@prettydamntired/test-lib',
 };
