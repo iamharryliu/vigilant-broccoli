9cacc175808b28e46fc496e59d33812dadfda04c
Harry Liu <harryliu1995@gmail.com>
2024-02-01 14:47:20 +0100

    refactor: vibecheck-lite-express

--- projects/nx-workspace/apps/api/vibecheck-lite-express/src/main.ts
+++ projects/nx-workspace/apps/api/vibecheck-lite-express/src/main.ts
@@ -2,12 +2,8 @@ import express from 'express';
 import cors from 'cors';
 import mongoose from 'mongoose';
 import { PORT, HOST, CORS_OPTIONS } from './app.const';
-import { VibecheckLite } from '@prettydamntired/vibecheck-lite';
 import { logger } from '@prettydamntired/test-node-tools';
-import {
-  HTTP_STATUS_CODES,
-  EmailSubscription,
-} from '@prettydamntired/test-lib';
+import { Controller } from './controller';
 
 const app = express();
 app.use(cors(CORS_OPTIONS));
@@ -16,64 +12,16 @@ app.get('/', (_, response) => {
   response.send('vibecheck-lite-express');
 });
 
-app.get('/get-outfit-recommendation', async (req, res) => {
-  const latitude = Number(req.query.lat);
-  const longitude = Number(req.query.lon);
-  const vibecheckLite = new VibecheckLite();
-  const recommendation = await vibecheckLite.getOutfitRecommendation({
-    latitude,
-    longitude,
-  });
-  return res.status(HTTP_STATUS_CODES.OK).json({ data: recommendation });
-});
-
-app.post('/subscribe', express.json(), async (req, res) => {
-  const { email, latitude, longitude } = req.body;
-  if (!email) {
-    return res
-      .status(HTTP_STATUS_CODES.BAD_REQUEST)
-      .json({ error: 'Email is required.' });
-  }
-  const emailSubscription = await EmailSubscription.findOne({
-    email: email,
-  });
-  const isSubscribed = !!emailSubscription;
-  if (!isSubscribed) {
-    const newEmailAlert = new EmailSubscription({
-      email,
-      latitude,
-      longitude,
-    });
-    await newEmailAlert.save();
-  } else {
-    await EmailSubscription.findOneAndUpdate(
-      {
-        email: email,
-      },
-      {
-        $set: {
-          latitude,
-          longitude,
-        },
-      },
-      { new: true },
-    );
-  }
-  return res.status(HTTP_STATUS_CODES.OK).json({});
-});
-
-app.delete('/unsubscribe/:email', express.json(), async (req, res) => {
-  const email = req.params.email;
-  await EmailSubscription.findOneAndDelete({ email });
-  return res.status(HTTP_STATUS_CODES.OK).json({});
-});
+app.get('/get-outfit-recommendation', Controller.getOutfitRecommendation);
+app.post('/subscribe', express.json(), Controller.subscribe);
+app.delete('/unsubscribe/:email', express.json(), Controller.unsubscribe);
 
 export const server = app.listen(PORT as number, HOST, () => {
   logger.info(`Server listening at ${HOST}:${PORT}`);
 });
 
 const MONGO_DB_SERVER = `mongodb+srv://${process.env.MONGO_DB_USERNAME}:${process.env.MONGO_DB_PASSWORD}@cluster0.txzecw2.mongodb.net`;
-mongoose.connect(MONGO_DB_SERVER, { dbName: 'vibecheck-lite' });
+mongoose.connect(MONGO_DB_SERVER, { dbName: 'vibecheck-lite-db' });
 export const db = mongoose.connection;
 db.getClient;
 db.on('error', error => {
---
+++ projects/nx-workspace/apps/api/vibecheck-lite-express/src/service.ts
@@ -0,0 +1,49 @@
+import { EmailSubscription, Location } from '@prettydamntired/test-lib';
+import { VibecheckLite } from '@prettydamntired/vibecheck-lite';
+
+export class Service {
+  static async getOutfitRecommendation(location: Location) {
+    const latitude = Number(location.latitude);
+    const longitude = Number(location.longitude);
+    const vibecheckLite = new VibecheckLite();
+    const recommendation = await vibecheckLite.getOutfitRecommendation({
+      latitude,
+      longitude,
+    });
+    return recommendation;
+  }
+
+  static async getIsSubscribed(email: string) {
+    return !!(await EmailSubscription.findOne({
+      email: email,
+    }));
+  }
+
+  static async subscribe(email: string, location: Location) {
+    const newEmailAlert = new EmailSubscription({
+      email,
+      latitude: location.latitude,
+      longitude: location.longitude,
+    });
+    await newEmailAlert.save();
+  }
+
+  static async updateSubscription(email: string, location: Location) {
+    await EmailSubscription.findOneAndUpdate(
+      {
+        email: email,
+      },
+      {
+        $set: {
+          latitude: location.latitude,
+          longitude: location.longitude,
+        },
+      },
+      { new: true },
+    );
+  }
+
+  static async unsubscribe(email: string) {
+    await EmailSubscription.findOneAndDelete({ email });
+  }
+}
