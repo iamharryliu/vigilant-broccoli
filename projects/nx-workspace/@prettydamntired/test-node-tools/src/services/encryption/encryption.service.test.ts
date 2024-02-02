28fb529ff51f6ba88e455f8b834e1b8fa6b0e684
Harry Liu <harryliu1995@gmail.com>
2024-02-02 13:03:06 +0100

    fix: fix default dotenvs

--- projects/nx-workspace/@prettydamntired/test-node-tools/src/services/encryption/encryption.service.test.ts
+++ projects/nx-workspace/@prettydamntired/test-node-tools/src/services/encryption/encryption.service.test.ts
@@ -4,5 +4,7 @@ describe('EncryptionService', () => {
   it('should work', () => {
     const encryptionService = new EncryptionService();
     expect(encryptionService).toBeTruthy();
+    expect(encryptionService.encryptData('test')).toBeTruthy();
+    expect(encryptionService.encryptData('test')).toBeTruthy();
   });
 });
