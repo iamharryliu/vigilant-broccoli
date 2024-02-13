882770b7b951699dc231d70420b3596dc05ec201
Harry Liu <harryliu1995@gmail.com>
2024-02-13 23:46:42 +0100

    chore: rename file

--- projects/nx-workspace/@prettydamntired/personal-website-api-lib/src/index.ts
+++ projects/nx-workspace/@prettydamntired/personal-website-api-lib/src/index.ts
@@ -1,3 +1,3 @@
 export * from './database/database.const';
 export * from './database/database.model';
-export * from './services/database-manager';
+export * from './services/database-manager.service';
