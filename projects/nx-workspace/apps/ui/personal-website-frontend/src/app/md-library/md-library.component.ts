95f65752f54ff228862f7107ae16757b3f2534d5
Harry Liu <harryliu1995@gmail.com>
2024-02-24 16:47:50 +0100

    feat(md-library): use general layout component

--- projects/nx-workspace/apps/ui/personal-website-frontend/src/app/md-library/md-library.component.ts
+++ projects/nx-workspace/apps/ui/personal-website-frontend/src/app/md-library/md-library.component.ts
@@ -2,11 +2,12 @@ import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FileService } from '../core/services/file.service';
 import { FolderItemComponent } from '../folder-item/folder-item.component';
+import { GeneralLayoutComponent } from '../components/layouts/general/genreral-layout.component';
 
 @Component({
   selector: 'app-md-library',
   standalone: true,
-  imports: [CommonModule, FolderItemComponent],
+  imports: [CommonModule, FolderItemComponent, GeneralLayoutComponent],
   templateUrl: './md-library.component.html',
 })
 export class MdLibraryComponent implements OnInit {
