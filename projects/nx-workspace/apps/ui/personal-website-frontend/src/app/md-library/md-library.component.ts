0a43346effa898a4d9cc39acf7a0f6a8664b2ca3
Harry Liu <harryliu1995@gmail.com>
2024-02-26 12:13:03 +0100

    refactor(md-library): rm return-to-top-button

--- projects/nx-workspace/apps/ui/personal-website-frontend/src/app/md-library/md-library.component.ts
+++ projects/nx-workspace/apps/ui/personal-website-frontend/src/app/md-library/md-library.component.ts
@@ -3,19 +3,13 @@ import { CommonModule } from '@angular/common';
 import { FileService } from '../core/services/file.service';
 import { FolderItemComponent } from '../folder-item/folder-item.component';
 import { GeneralLayoutComponent } from '../components/layouts/general/genreral-layout.component';
-import { ReturnTopButtonComponent } from '../components/global/return-top-button/return-top-button.component';
 import { Observable } from 'rxjs';
 import { AppService } from '../core/services/app.service';
 
 @Component({
   selector: 'app-md-library',
   standalone: true,
-  imports: [
-    CommonModule,
-    FolderItemComponent,
-    GeneralLayoutComponent,
-    ReturnTopButtonComponent,
-  ],
+  imports: [CommonModule, FolderItemComponent, GeneralLayoutComponent],
   templateUrl: './md-library.component.html',
   styleUrl: './md-library.component.scss',
   encapsulation: ViewEncapsulation.None,
