e841c3ae390e75106a013837ee7f5808581f606d
Harry Liu <harryliu1995@gmail.com>
2024-02-29 16:11:43 +0100

    feat: add new gc alias

--- setup/dotfiles/zsh/.aliases.zsh
+++ setup/dotfiles/zsh/.aliases.zsh
@@ -23,7 +23,7 @@ alias gb='git branch'
 alias gco='git checkout'
 alias gbswitch='git checkout -'
 alias gcob='git checkout -b'
-alias gc='git commit -m'
+alias gcm='git commit -m'
 alias gpush='git push'
 alias greset='git reset HEAD^'
 alias undocommit='greset --soft'
@@ -120,3 +120,26 @@ alias deploypwui="cdnx && nx deploy personal-website-frontend --skip-nx-cache"
 
 # Journal
 alias pushJournal="cd ~/journal && git add . && git commit -m 'docs: update journal' && git push"
+
+# Git Commit
+
+function gc() {
+  if [ "$#" -lt 2 ]; then
+    echo "Usage: $0 <git_type> [<project>] <message>"
+    exit 1
+  fi
+  git_type="$1"
+  project=""
+  message=""
+  if [ "$#" -eq 3 ]; then
+    project="$2"
+    message="$3"
+    commit_message="$git_type($project): $message"
+  else
+    project=""
+    message="$2"
+    commit_message="$git_type: $message"
+  fi
+  git commit -m "$commit_message"
+  echo "$commit_message"
+}
