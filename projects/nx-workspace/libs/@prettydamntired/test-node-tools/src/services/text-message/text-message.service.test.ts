edbe2cb4a1dda2303bb45817215001d8d894acc6
Harry Liu <harryliu1995@gmail.com>
2024-03-05 23:44:43 +0100

    build(github-actions): add test back

--- projects/nx-workspace/libs/@prettydamntired/test-node-tools/src/services/text-message/text-message.service.test.ts
+++ projects/nx-workspace/libs/@prettydamntired/test-node-tools/src/services/text-message/text-message.service.test.ts
@@ -1,8 +1,8 @@
-// import { TextMessageService } from './text-message.service';
+import { TextMessageService } from './text-message.service';
 
 describe('TextMessageService', () => {
-  // it('should be created', () => {
-  //   const textMessageService = new TextMessageService();
-  //   expect(textMessageService).toBeTruthy();
-  // });
+  it('should be created', () => {
+    const textMessageService = new TextMessageService();
+    expect(textMessageService).toBeTruthy();
+  });
 });
