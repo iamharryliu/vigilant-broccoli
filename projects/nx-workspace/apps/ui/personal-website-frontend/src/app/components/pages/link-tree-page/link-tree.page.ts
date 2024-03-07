8ad0b702de196fcc9fa512ec8eae99b37d2300e3
Harry Liu <harryliu1995@gmail.com>
2024-03-07 14:07:10 +0100

    feat(link-tree): update link tree page

--- projects/nx-workspace/apps/ui/personal-website-frontend/src/app/components/pages/link-tree-page/link-tree.page.ts
+++ projects/nx-workspace/apps/ui/personal-website-frontend/src/app/components/pages/link-tree-page/link-tree.page.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 import { LINKS } from '../../../core/consts/app-route.const';
 import { CardComponent } from '../../global/card/card.component';
 import { CenteredAppLayoutComponent } from '../../layouts/centered-app-layout/centered-app-layout.compoenent';
-import { ButtonLinkComponent } from 'general-components';
+import { ButtonLinkComponent, Link } from 'general-components';
 
 @Component({
   standalone: true,
@@ -15,13 +15,18 @@ import { ButtonLinkComponent } from 'general-components';
     CommonModule,
     CardComponent,
   ],
+  styles: [
+    '#profile-picture {width: 120px; height: 120px; border-radius: 50%;}',
+  ],
 })
 export class LinkTreePageComponent {
-  LINKS = [
-    LINKS.INDEX_PAGE,
+  LINKS: Link[] = [
+    { ...LINKS.INDEX_PAGE, text: 'Personal Website' },
+    LINKS.LINKEDIN,
+    LINKS.GITHUB,
+    LINKS.RESUME,
     LINKS.PERSONAL_INSTAGRAM,
     LINKS.SECONDHAND_STORE_IG,
     LINKS.SKATE_IG,
   ];
-  CAREER_LINKS = [LINKS.LINKEDIN, LINKS.GITHUB, LINKS.RESUME, LINKS.ABOUT_PAGE];
 }
