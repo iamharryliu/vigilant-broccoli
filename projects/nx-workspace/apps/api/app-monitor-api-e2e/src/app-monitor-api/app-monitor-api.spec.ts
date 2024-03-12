61db2edaf9f51c7e0a1d407816cc89957ffb8493
Harry Liu <harryliu1995@gmail.com>
2024-03-12 08:53:26 -0400

    chore: remove app-monitor-specs

--- projects/nx-workspace/apps/api/app-monitor-api-e2e/src/app-monitor-api/app-monitor-api.spec.ts
+++
@@ -1,8 +0,0 @@
-import axios from 'axios';
-
-describe('GET /', () => {
-  it('should return a message', async () => {
-    const res = await axios.get(`/`);
-    expect(res.status).toBe(200);
-  });
-});
