c7f7945b356532a95b7a43b8cfbcdc09ef49f41a
Harry Liu <harryliu1995@gmail.com>
2024-03-12 18:25:51 -0400

    feat(aliases): update .aliases.zsh

--- setup/dotfiles/zsh/.aliases.zsh
+++ setup/dotfiles/zsh/.aliases.zsh
@@ -6,6 +6,7 @@ alias openyoutube='open -a "Google Chrome" "https://www.youtube.com/"'
 alias openudemy='open -a "Google Chrome" "https://www.udemy.com/"'
 alias openmemrise='open -a "Google Chrome" "https://app.memrise.com/dashboard"'
 alias openamazon='open -a "Google Chrome" "https://www.amazon.ca/"'
+alias openchatgpt='open -a "Google Chrome" "https://chat.openai.com/"'
 alias google="~/shell-scripts/google_search.sh"
 alias youtube="~/shell-scripts/youtube_search.sh"
 alias amazon="~/shell-scripts/amazon_search.sh"
