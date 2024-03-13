92cfaaf0687f8285e5093ab20fcd43d388caa5e4
Harry Liu <harryliu1995@gmail.com>
2024-03-13 07:24:51 -0400

    docs: update dotfiles

--- setup/dotfiles/zsh/aliases/quick_link_aliases.sh
+++ setup/dotfiles/zsh/aliases/quick_link_aliases.sh
@@ -1,11 +1,20 @@
+# General Links
 alias opengmail='open -a "Google Chrome" "https://gmail.com/"'
 alias openyoutube='open -a "Google Chrome" "https://www.youtube.com/"'
 alias openudemy='open -a "Google Chrome" "https://www.udemy.com/"'
-alias openmemrise='open -a "Google Chrome" "https://app.memrise.com/dashboard"'
 alias openamazon='open -a "Google Chrome" "https://www.amazon.ca/"'
 alias openchatgpt='open -a "Google Chrome" "https://chat.openai.com/"'
 alias google="~/shell-scripts/google_search.sh"
 alias youtube="~/shell-scripts/youtube_search.sh"
 alias amazon="~/shell-scripts/amazon_search.sh"
+
+# Language Learning
+alias openmemrise='open -a "Google Chrome" "https://app.memrise.com/dashboard"'
 alias udemyspanish='open -a "Google Chrome" "https://www.udemy.com/course/3-minute-spanish-course-5/learn"'
 alias udemyfrench='open -a "Google Chrome" "https://www.udemy.com/course/3-minute-french-course-8/learn"'
+alias languagelearn="openmemrise && udemyspanish && udemyfrench"
+
+# Career
+alias openresume='open -a "Google Chrome" "https://docs.google.com/document/d/1s6Wy8i4zU85o19qyXKhdpH4jdTP36QDPUgZdV7E6-QU/edit#heading=h.uzt44hq0695d"'
+alias openlinkedin='open -a "Google Chrome" "https://www.linkedin.com/jobs/"'
+alias jobhunt='openresume && openlinkedin'
