6ad22e7102134a73b3949dd1815d468ea8680273
Harry Liu <harryliu1995@gmail.com>
2024-03-12 21:31:22 -0400

    feat(aliases): update .aliases.zsh

--- setup/dotfiles/zsh/aliases/dir_aliases.sh
+++ setup/dotfiles/zsh/aliases/dir_aliases.sh
@@ -1,6 +1,9 @@
 alias ..='cd ..'
 alias ..x2='cd ../..'
 alias ..x3='cd ../../..'
+newfile() {
+    mkdir -p "$(dirname "$1")" && touch "$1" && code "$1"
+}
 alias mkdir='mkdir -pv'
 alias mkcd='function _mkcd() { mkdir -p "$1" && cd "$1"; }; _mkcd'
 alias ls='ls -FGhl'
