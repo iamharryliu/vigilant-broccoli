edb9c3bdbd6da9d106f9cd42d580c1634757aa5b
Harry Liu <harryliu1995@gmail.com>
2024-03-13 06:03:56 -0400

    feat(aliases): update .aliases.zsh

--- setup/dotfiles/zsh/aliases/quick_link_aliases.sh
+++ setup/dotfiles/zsh/aliases/quick_link_aliases.sh
@@ -7,3 +7,5 @@ alias openchatgpt='open -a "Google Chrome" "https://chat.openai.com/"'
 alias google="~/shell-scripts/google_search.sh"
 alias youtube="~/shell-scripts/youtube_search.sh"
 alias amazon="~/shell-scripts/amazon_search.sh"
+alias udemyspanish='open -a "Google Chrome" "https://www.udemy.com/course/3-minute-spanish-course-5/learn"'
+alias udemyfrench='open -a "Google Chrome" "https://www.udemy.com/course/3-minute-french-course-8/learn"'
