f49364897fdde41df73fbb04776e78e8dd5827fb
Harry Liu <harryliu1995@gmail.com>
2024-03-15 18:46:17 -0400

    docs: update dotfiles

--- setup/dotfiles/zsh/aliases/python_aliases.sh
+++ setup/dotfiles/zsh/aliases/python_aliases.sh
@@ -1,7 +1,7 @@
 
 alias python=python3
 alias newvenv='rm -r venv; python -m venv venv; source venv/bin/activate'
-alias makevenv='rm -r venv; python -m venv venv; source venv/bin/activate; pip install -r requirements.txt'
+alias makevenv='deactivate; rm -r venv; python -m venv venv; source venv/bin/activate; pip install -r requirements.txt'
 alias runvenv='source venv/bin/activate; python run.py'
 alias venvon='source venv/bin/activate'
 alias pipdump='pip freeze  > requirements.txt'
