ded02193ee439222bbfd7d1fc7f1a82d27244ca5
Harry Liu <harryliu1995@gmail.com>
2024-03-16 06:22:46 -0400

    feat(dotfiles): add dotfile template

---
+++ setup/dotfiles/zsh/.env.sh
@@ -0,0 +1,35 @@
+export PORT=3000
+
+# Email
+export MY_EMAIL="email"
+export MY_EMAIL_PASSWORD="password"
+
+# Mongo
+export MONGO_DB_USERNAME="username"
+export MONGO_DB_PASSWORD="password"
+
+# Recaptcha
+export RECAPTCHA_V3_SECRET_KEY="secretKey"
+
+# OpenWeather
+export OPENWEATHER_API_KEY="secretKey"
+export OPENAI_API_KEY="secretKey"
+
+# Twilio
+export TWILIO_ACCOUNT_SID="accountSid"
+export TWILIO_AUTH_TOKEN="accountAuthToken"
+export TWILIO_PHONE_NUMBER="phoneNumber"
+export TWILIO_DEFAULT_TO_NUMBER="phoneNumber"
+
+# AWS
+export ec2_instance='instance'
+export ec2_instance_username_ip_address='[username]@[ipAddress]'
+export AWS_CLI_AUTO_PROMPT='on-partial'
+
+# Cloudflare
+export CLOUDFLARE_ID='cloudflareId'
+export AWS_ACCESS_KEY_ID='accessKey'
+export AWS_SECRET_ACCESS_KEY='secretAccessKey'
+
+# Vibecheck
+export VIBECHECK_SQL_DB="postgresql://postgres:[password]@localhost:5432/[bucketName]"
