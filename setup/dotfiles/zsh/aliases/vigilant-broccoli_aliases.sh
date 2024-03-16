6e1c3479f94480acc6792d20a49f583fedd2dd5b
Harry Liu <harryliu1995@gmail.com>
2024-03-16 06:21:35 -0400

    docs: update dotfiles

--- setup/dotfiles/zsh/aliases/vigilant-broccoli_aliases.sh
+++ setup/dotfiles/zsh/aliases/vigilant-broccoli_aliases.sh
@@ -6,7 +6,7 @@ alias cdnx='cd ~/vigilant-broccoli/projects/nx-workspace/'
 alias grind75='python -m unittest discover -s ~/vigilant-broccoli/projects/grind-75'
 alias pushtodo="cdvb && git add TODO.md && gc docs 'update TODO.md' && gpush"
 alias pushmd="cdvb && git add README.md TODO.md snippets/*.md notes/**/*.md && gc docs 'update md files' && gpush"
-alias pushdotfiles="cdvb && git add setup/dotfiles && gc docs 'update dotfiles' && gpush"
+alias pushdotfiles="cdvb && git add setup/dotfiles && git reset setup/dotfiles/zsh/.env.sh && gc docs 'update dotfiles' && gpush"
 # Journal
 alias pushJournal="cd ~/journal && git add . && gc docs 'update journal' && gpush"
 
