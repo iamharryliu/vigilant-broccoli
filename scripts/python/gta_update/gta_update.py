d0dd2292d5ca97cde072bf8224cc9d6cbbc617a1
Harry Liu <harryliu1995@gmail.com>
2024-03-18 21:04:01 -0400

    feat(python-mail-handler): enhance mail handler and add gta_update script

---
+++ scripts/python/gta_update/gta_update.py
@@ -0,0 +1,66 @@
+import threading
+import requests
+from bs4 import BeautifulSoup
+import sys
+
+sys.path.append("..")
+from tools.mail_handler import MailHandler
+
+
+url = "https://gtaupdate.com/"
+key_words = ["GALLOWAY RD", "KINGSTON RD", "LAWRENCE AVE"]
+emails = ["harryliu1995@gmail.com"]
+
+
+class HTMLPageParser:
+    def get_row_data(url, text):
+        response = requests.get(url)
+        if response.status_code != 200:
+            print("Failed to fetch the webpage:", response.status_code)
+            return
+        row_data = ""
+        soup = BeautifulSoup(response.text, "html.parser")
+        tables = soup.find_all("table")
+        for table in tables:
+            rows = table.find_all("tr")
+            for row in rows:
+                cells = row.find_all("td")
+                for cell in cells:
+                    if text in cell.get_text():
+                        row_data = [cell.get_text().strip() for cell in cells]
+        return row_data
+
+    def add_unique_result(url, key_word, results):
+        result = HTMLPageParser.get_row_data(url, key_word)
+        if result not in results:
+            results.append(result)
+
+    def get_rows_from_html():
+        results = []
+        threads = []
+        for key_word in key_words:
+            thread = threading.Thread(
+                target=lambda: results.append(
+                    HTMLPageParser.add_unique_result(url, key_word, results)
+                )
+            )
+            threads.append(thread)
+            thread.start()
+
+        for thread in threads:
+            thread.join()
+
+        return results
+
+
+results = HTMLPageParser.get_rows_from_html()
+if results:
+    results = [result for result in results if result]
+    MailHandler.email_to_list(
+        emails,
+        message={
+            "from": "GTA Update",
+            "subject": "GTA Update",
+            "body": MailHandler.format_for_email(results),
+        },
+    )
--- scripts/python/tools/mail_handler/__init__.py
+++ scripts/python/tools/mail_handler/__init__.py
@@ -1,12 +1,26 @@
 import os
 import smtplib
+import threading
 
 EMAIL_ADDRESS = os.environ.get("MY_EMAIL")
 EMAIL_PASSWORD = os.environ.get("MY_EMAIL_PASSWORD")
 
+DEFAULT_SEND_TO_SELF_EMAIL = {
+    "from": "yourself",
+    "to": EMAIL_ADDRESS,
+    "subject": "subject",
+    "body": "body",
+}
+
 
 class MailHandler:
-    def email_to_self(email={"subject": "subject", "body": "body"}):
+    def format_for_email(list_of_lists):
+        formatted_text = "\n\n".join(
+            [" ".join(map(str, sublist)) for sublist in list_of_lists]
+        )
+        return formatted_text
+
+    def send_email(email):
         with smtplib.SMTP("smtp.gmail.com", 587) as smtp:
             smtp.ehlo()
             smtp.starttls()
@@ -15,4 +29,20 @@ class MailHandler:
             subject = email["subject"]
             body = email["body"]
             message = f"Subject: {subject}\n\n{body}"
-            smtp.sendmail(EMAIL_ADDRESS, EMAIL_ADDRESS, message)
+            smtp.sendmail(email["from"], email["to"], message)
+
+    def email_to_self(message=DEFAULT_SEND_TO_SELF_EMAIL):
+        MailHandler.send_email({**message, "to": EMAIL_ADDRESS})
+
+    def email_to_list(
+        emails, message={"from": "Mail Handler", "subject": "subject", "body": "body"}
+    ):
+        emails = [{**message, "to": email} for email in emails]
+        threads = []
+        for email in emails:
+            thread = threading.Thread(target=MailHandler.send_email, args=(email,))
+            threads.append(thread)
+            thread.start()
+
+        for thread in threads:
+            thread.join()
