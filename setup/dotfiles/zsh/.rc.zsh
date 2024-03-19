ddd949bc8443d3b88775a285f0c3275f8ce8792e
Harry Liu <harryliu1995@gmail.com>
2024-03-19 08:21:03 -0400

    docs: update dotfiles

--- setup/dotfiles/zsh/.rc.zsh
+++ setup/dotfiles/zsh/.rc.zsh
@@ -4,12 +4,9 @@ source ~/.env.sh
 
 export PATH="/opt/homebrew/bin:$PATH"
 if [[ $(sysctl -n machdep.cpu.brand_string | grep -c "Apple") -gt 0 ]]; then
-    # Running on Apple Silicon (M series)
-    echo "Running on Apple Silicon (M series)"
+    # echo "Setting zsh-autosuggestions for Apple Silicon (M series)"
     source /opt/homebrew/share/zsh-autosuggestions/zsh-autosuggestions.zsh
 else
-    # Running on Intel
-    echo "Running on Intel"
+    # echo "Setting zsh-autosuggestions for macOS Intel"
     source /usr/local/share/zsh-autosuggestions/zsh-autosuggestions.zsh
 fi
-
