6d3e071a91ebd4cc73e3d0ffc93223c9f2634c26
Harry Liu <harryliu1995@gmail.com>
2024-03-19 00:08:09 -0400

    feat(gta-update-alert): enhance script to scrape only for past hour

--- scripts/python/gta_update/gta_update.py
+++ scripts/python/gta_update/gta_update.py
@@ -1,6 +1,7 @@
-import threading
+from datetime import datetime, timedelta
 import requests
 from bs4 import BeautifulSoup
+from pytz import timezone
 import sys
 
 sys.path.append("..")
@@ -8,7 +9,6 @@ from tools.mail_handler import MailHandler
 
 
 url = "https://gtaupdate.com/"
-key_words = ["GALLOWAY RD", "KINGSTON RD", "LAWRENCE AVE"]
 emails = [
     "harryliu1995@gmail.com",
     "dnchanners@gmail.com",
@@ -17,47 +17,41 @@ emails = [
 
 
 class HTMLPageParser:
-    def get_row_data(url, text):
+    def get_recent_alerts():
         response = requests.get(url)
-        if response.status_code != 200:
-            print("Failed to fetch the webpage:", response.status_code)
-            return
-        row_data = ""
-        soup = BeautifulSoup(response.text, "html.parser")
-        tables = soup.find_all("table")
-        for table in tables:
-            rows = table.find_all("tr")
-            for row in rows:
-                cells = row.find_all("td")
-                for cell in cells:
-                    if text in cell.get_text():
-                        row_data = [cell.get_text().strip() for cell in cells]
-        return row_data
+        if response.status_code == 200:
+            soup = BeautifulSoup(response.content, "html.parser")
+            table = soup.find("table")
+            if table:
+                rows = table.find_all("tr")
+                table_data = []
+                for row in rows[1:]:
+                    cells = row.find_all(["th", "td"])
+                    row_data = [cell.get_text(strip=True) for cell in cells]
 
-    def add_unique_result(url, key_word, results):
-        result = HTMLPageParser.get_row_data(url, key_word)
-        if result not in results:
-            results.append(result)
-
-    def get_rows_from_html():
-        results = []
-        threads = []
-        for key_word in key_words:
-            thread = threading.Thread(
-                target=lambda: results.append(
-                    HTMLPageParser.add_unique_result(url, key_word, results)
-                )
-            )
-            threads.append(thread)
-            thread.start()
-
-        for thread in threads:
-            thread.join()
-
-        return results
+                    current_time_est = datetime.now(timezone("America/New_York"))
+                    past_hour_est = current_time_est - timedelta(hours=1)
+                    if convert_to_est(row_data[0]) >= past_hour_est:
+                        table_data.append(row_data)
+                return table_data
+        else:
+            print(f"Failed to fetch URL: {url}")
 
 
-results = HTMLPageParser.get_rows_from_html()
+def convert_to_est(time_str):
+    today_date = datetime.now().date()
+    if "-" in time_str:
+        datetime_str = f"{time_str} {today_date.year}"
+        time_obj = datetime.strptime(datetime_str, "%b-%d %I:%M %p %Y")
+    else:
+        datetime_str = f"{today_date} {time_str}"
+        time_obj = datetime.strptime(datetime_str, "%Y-%m-%d %I:%M %p")
+    est = timezone("America/New_York")
+    time_obj = est.localize(time_obj)
+    return time_obj
+
+
+results = HTMLPageParser.get_recent_alerts()
 if results:
     results = [result for result in results if result]
     MailHandler.email_to_list(
