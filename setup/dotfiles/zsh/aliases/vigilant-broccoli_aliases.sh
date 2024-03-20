91934d2a9709cf33b5a1177bb8f292b3798f2eae
Harry Liu <harryliu1995@gmail.com>
2024-03-20 10:54:29 -0400

    docs: update dotfiles

--- setup/dotfiles/zsh/aliases/vigilant-broccoli_aliases.sh
+++ setup/dotfiles/zsh/aliases/vigilant-broccoli_aliases.sh
@@ -9,7 +9,7 @@ alias commitmd="cdvb && git add README.md TODO.md snippets/*.md notes/**/*.md &&
 alias pushmd="commitmd && gpush"
 alias commitdotfiles="cdvb && git add setup/dotfiles && git reset setup/dotfiles/zsh/.env.sh && gc docs 'update dotfiles'"
 alias pushdotfiles="commitdotfiles && gpush"
-alias pushactions="cdvb && git add *.yml && gc build github-actions 'update actions' && gpush"
+alias pushactions="cdvb && git add .github/workflows && gc build github-actions 'update actions' && gpush"
 alias dldjmusic="cd ~/$REPO_NAME/scripts/python/dj-scripts/ && source venv/bin/activate && python spotify_to_dj_library.py"
 
 # Journal
