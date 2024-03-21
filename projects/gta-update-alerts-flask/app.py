0c19d135c8f7f272c4445c1612613643ad9c8c3a
Harry Liu <harryliu1995@gmail.com>
2024-03-21 19:07:53 -0400

    feat(gta-update-alert): enhance filtering for districts and update styling

--- projects/gta-update-alerts-flask/app.py
+++ projects/gta-update-alerts-flask/app.py
@@ -5,6 +5,39 @@ import psycopg2
 app = Flask(__name__)
 
 DATABASE_URL = os.environ.get("GTA_UPDATE_ALERTS_DB")
+import requests
+from bs4 import BeautifulSoup
+
+url = "https://en.wikipedia.org/wiki/Toronto_Police_Service"
+response = requests.get(url)
+soup = BeautifulSoup(response.text, "html.parser")
+TPS_DATA = []
+for i in range(3, 5):
+    for li in soup.find_all("div", class_="div-col")[i].find_all("li"):
+        text = li.text.strip()
+        TPS_DATA.append(li.text.strip().split(","))
+TPS_DIVISIONS = [
+    {"name": division[0], "location": division[1]} for division in TPS_DATA
+]
+print(len(TPS_DIVISIONS) == 16)
+
+url = "https://en.wikipedia.org/wiki/Toronto_Fire_Services"
+response = requests.get(url)
+soup = BeautifulSoup(response.text, "html.parser")
+TFS_DATA = []
+for i in range(2, 6):
+    table = soup.find_all("table", class_="wikitable")[i]
+    for row in table.find_all("tr")[1:]:
+        row_data = []
+        cells = row.find_all(["th", "td"])
+        row_data.extend(cell.get_text(strip=True) for cell in cells)
+        TFS_DATA.append(row_data)
+print(len(TFS_DATA) == 84)
+
+TFS_STATIONS = [
+    {"name": f"TFS {station[0]}", "district": station[1], "location": station[8]}
+    for station in TFS_DATA
+]
 
 
 def get_db_connection():
@@ -13,7 +46,9 @@ def get_db_connection():
 
 @app.route("/")
 def index():
-    return render_template("index.html")
+    return render_template(
+        "index.html", TPS_DIVISIONS=TPS_DIVISIONS, TFS_STATIONS=TFS_STATIONS
+    )
 
 
 @app.post("/submit")
