2f3a8050c3438c9706e5bc370160278f2c3438a8
Harry Liu <harryliu1995@gmail.com>
2024-03-22 14:04:23 -0400

    refactor(spotify-to-mp3): refactor to get playlists from json file

--- scripts/python/dj-scripts/spotify_to_dj_library.py
+++ scripts/python/dj-scripts/spotify_to_dj_library.py
@@ -1,46 +1,10 @@
+import json
 from spotify_to_mp3_service import SpotifyToMp3Service
 
 
 def main():
-    playlists = [
-        {
-            "name": "something fun",
-            "url": "https://open.spotify.com/playlist/02Fku7GHNdgm02W4RGTjdG",
-        },
-        {
-            "name": "eurobeats",
-            "url": "https://open.spotify.com/playlist/0GY6Cnx6NgpjbCuxFMvROD",
-        },
-        {
-            "name": "vibes",
-            "url": "https://open.spotify.com/playlist/2SmVVHqYt8IyoQWwh3Eja1",
-        },
-        {
-            "name": "edgy beats",
-            "url": "https://open.spotify.com/playlist/1YuKYIViNWO4n9PwUPpA93",
-        },
-        {
-            "name": "dnd",
-            "url": "https://open.spotify.com/playlist/6qk9QGFUdo3prDKqZH5SeR",
-        },
-        {
-            "name": "religious",
-            "url": "https://open.spotify.com/playlist/2WGUADorsMlsJz6KrA6I1G",
-        },
-        {
-            "name": "chill house mix",
-            "url": "https://open.spotify.com/playlist/0U9BCLabfVnNJl9lbmNhmJ",
-        },
-        {
-            "name": "cracked out",
-            "url": "https://open.spotify.com/playlist/7rRl4iWKuBRDYPxxNnDkt0",
-        },
-        {
-            "name": "deep",
-            "url": "https://open.spotify.com/playlist/1OibleLCNpgnUWxaSCs9tR",
-        },
-    ]
-
+    with open("playlists.json", "r") as file:
+        playlists = json.load(file)
     service = SpotifyToMp3Service()
     service.download_playlists(playlists)
 
