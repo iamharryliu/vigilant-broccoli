750c9f8bcedd91165fea81c0ce0ebc0649586868
Harry Liu <harryliu1995@gmail.com>
2024-03-22 15:08:17 -0400

    build(cron): rc file check for cron flag

--- setup/dotfiles/zsh/.rc.zsh
+++ setup/dotfiles/zsh/.rc.zsh
@@ -4,10 +4,15 @@ source ~/.zsh_aliases
 source ~/.env.sh
 
 export PATH="/opt/homebrew/bin:$PATH"
-if [[ $(sysctl -n machdep.cpu.brand_string | grep -c "Apple") -gt 0 ]]; then
-    # echo "Setting zsh-autosuggestions for Apple Silicon (M series)"
-    source /opt/homebrew/share/zsh-autosuggestions/zsh-autosuggestions.zsh
+
+if [ "$IS_CRON" = "true" ]; then
+    echo "Script is being run by cron"
 else
-    # echo "Setting zsh-autosuggestions for macOS Intel"
-    source /usr/local/share/zsh-autosuggestions/zsh-autosuggestions.zsh
+    if [[ $(sysctl -n machdep.cpu.brand_string | grep -c "Apple") -gt 0 ]]; then
+        # echo "Setting zsh-autosuggestions for Apple Silicon (M series)"
+        source /opt/homebrew/share/zsh-autosuggestions/zsh-autosuggestions.zsh
+    else
+        # echo "Setting zsh-autosuggestions for macOS Intel"
+        source /usr/local/share/zsh-autosuggestions/zsh-autosuggestions.zsh
+    fi
 fi
