c5a43aaf52c7fe8d792289c2c341c77d37ea470c
Harry Liu <harryliu1995@gmail.com>
2024-03-25 14:09:07 -0400

    feat(personal-website): add link tree to header

--- projects/nx-workspace/apps/ui/personal-website-frontend/src/app/components/layouts/general/genreral-layout.component.ts
+++ projects/nx-workspace/apps/ui/personal-website-frontend/src/app/components/layouts/general/genreral-layout.component.ts
@@ -1,12 +1,14 @@
 import { CommonModule } from '@angular/common';
 import { Component, Input } from '@angular/core';
 import { NavbarComponent } from '../../features/navbar/navbar.component';
+import { LINKS } from '../../../core/consts/app-route.const';
+import { RouterModule } from '@angular/router';
 
 @Component({
   standalone: true,
   selector: 'app-general-layout',
   templateUrl: './general-layout.component.html',
-  imports: [CommonModule, NavbarComponent],
+  imports: [CommonModule, NavbarComponent, RouterModule],
   styles: [
     '#profile-picture {width: 120px; height: 120px; border-radius: 50%;}',
   ],
@@ -14,4 +16,5 @@ import { NavbarComponent } from '../../features/navbar/navbar.component';
 export class GeneralLayoutComponent {
   @Input() hasContactForm = true;
   @Input() hasSubscribeForm = true;
+  LINKS = LINKS;
 }
