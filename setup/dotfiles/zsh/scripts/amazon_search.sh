fdf6143bd8a33e0f4b40fe514a8f14854c318e4b
Harry Liu <harryliu1995@gmail.com>
2024-03-26 14:33:58 -0400

    docs: update dotfiles

--- setup/dotfiles/zsh/scripts/amazon_search.sh
+++ setup/dotfiles/zsh/scripts/amazon_search.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 search_query=$(echo "$*" | sed 's/ /+/g')
-search_url="https://www.amazon.com/s?k=$search_query"
+search_url="https://www.amazon.ca/s?k=$search_query"
 open "$search_url"
