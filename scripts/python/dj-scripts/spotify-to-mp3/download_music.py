730f487d50622244e473b1454449cade09025be8
Harry Liu <harryliu1995@gmail.com>
2024-04-02 14:10:51 -0400

    feat(dj-tools): update script to use default and custom playlist files

--- scripts/python/dj-scripts/spotify-to-mp3/download_music.py
+++ scripts/python/dj-scripts/spotify-to-mp3/download_music.py
@@ -1,10 +1,18 @@
 import json
+import os
 from spotify_to_mp3_service import SpotifyToMp3Service
+
+CUSTOM_PLAYLIST_FILENAME = "custom_playlists.json"
+DEFAULT_PLAYLIST_FILENAME = "default_playlists.json"
 
 
 def main():
-    with open("playlists.json", "r") as file:
-        playlists = json.load(file)
+    if os.path.exists(CUSTOM_PLAYLIST_FILENAME):
+        with open(CUSTOM_PLAYLIST_FILENAME, "r") as file:
+            playlists = json.load(file)
+    else:
+        with open(DEFAULT_PLAYLIST_FILENAME, "r") as file:
+            playlists = json.load(file)
     service = SpotifyToMp3Service()
     service.download_playlists(playlists)
 
