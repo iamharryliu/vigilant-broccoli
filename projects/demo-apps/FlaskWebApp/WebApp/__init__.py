7f6454d41deb854eec2658f8a9cfc638d7905ed5
Harry Liu <harryliu1995@gmail.com>
2024-04-11 14:41:33 -0400

    chore(flask-demo): drop models

--- projects/demo-apps/FlaskWebApp/WebApp/__init__.py
+++ projects/demo-apps/FlaskWebApp/WebApp/__init__.py
@@ -4,7 +4,6 @@ from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
 from flask_mail import Mail
 from flask_admin import Admin
-from WebApp.config import Config
 from WebApp.admin import MyAdminIndexView, ModelView
 
 db = SQLAlchemy()
@@ -18,7 +17,7 @@ admin = Admin(index_view=MyAdminIndexView())
 from WebApp.models import *
 
 
-def create_app(config_class=Config):
+def create_app(Config):
     app = Flask(__name__)
     app.url_map.strict_slashes = False
     app.config.from_object(Config)
@@ -31,13 +30,7 @@ def create_app(config_class=Config):
     with app.app_context():
         db.create_all()
 
-    admin.add_view(ModelView(Subscriber, db.session))
     admin.add_view(ModelView(User, db.session))
-    admin.add_view(ModelView(Post, db.session))
-    admin.add_view(ModelView(Product, db.session))
-    admin.add_view(ModelView(Cart, db.session))
-    admin.add_view(ModelView(Item, db.session))
-    admin.add_view(ModelView(Order, db.session))
 
     from WebApp.main.routes import main_blueprint
     from WebApp.users.routes import users_blueprint
