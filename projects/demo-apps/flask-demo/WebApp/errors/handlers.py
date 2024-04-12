5537d5a8c4d68d57f6e19a7691f0ef0a6cc9fc1d
Harry Liu <harryliu1995@gmail.com>
2024-04-12 13:06:58 -0400

    refacator(flask-demo): refactor error handlers

--- projects/demo-apps/flask-demo/WebApp/errors/handlers.py
+++ projects/demo-apps/flask-demo/WebApp/errors/handlers.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, jsonify
+from flask import Blueprint
 
 errors_blueprint = Blueprint("errors", __name__)
 
@@ -6,28 +6,28 @@ errors_blueprint = Blueprint("errors", __name__)
 # Bad request.
 @errors_blueprint.app_errorhandler(400)
 def error_400(error):
-    return jsonify(error=str(error)), 400
+    return {"error": str(error)}, 400
 
 
 # Unauthorized request.
 @errors_blueprint.app_errorhandler(401)
 def error_401(error):
-    return jsonify(error=str(error)), 401
+    return {"error": str(error)}, 401
 
 
 # Forbidden request.
 @errors_blueprint.app_errorhandler(403)
 def error_403(error):
-    return jsonify(error=str(error)), 403
+    return {"error": str(error)}, 403
 
 
 # Not found error.
 @errors_blueprint.app_errorhandler(404)
 def error_404(error):
-    return jsonify(error=str(error)), 404
+    return {"error": str(error)}, 404
 
 
 # Internal server error.
 @errors_blueprint.app_errorhandler(500)
 def error_500(error):
-    return jsonify(error=str(error)), 500
+    return {"error": str(error)}, 500
