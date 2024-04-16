0c4c997489f1948f5b35da6105286c6f80b90883
Harry Liu <harryliu1995@gmail.com>
2024-04-16 13:41:38 -0400

    feat(aliases): update aliases

--- setup/dotfiles/zsh/aliases/python_aliases.sh
+++ setup/dotfiles/zsh/aliases/python_aliases.sh
@@ -1,7 +1,7 @@
 alias python=python3
 alias newvenv='rm -r venv; python -m venv venv; source venv/bin/activate'
 alias newvenv='deactivate; rm -r venv; python -m venv venv; source venv/bin/activate'
-alias makevenv='rm -r venv; python -m venv venv; source venv/bin/activate; if [ -f requirements.txt ]; then pip install -r requirements.txt; else echo "No requirements.txt found, skipping installation."; fi; pip freeze > requirements.txt'
+alias makevenv='if [ -d venv ]; then rm -r venv; fi; python -m venv venv; source venv/bin/activate; if [ -f requirements.txt ]; then pip install -r requirements.txt; else echo "No requirements.txt found, skipping installation."; fi; pip freeze > requirements.txt'
 alias runvenv='source venv/bin/activate; python run.py'
 alias venvon='source venv/bin/activate'
 alias revenv='initsh && venvon'
