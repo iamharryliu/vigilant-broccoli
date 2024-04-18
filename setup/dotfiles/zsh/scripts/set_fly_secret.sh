22093d999dd37542757250cc86d91562eb4e8a07
Harry Liu <harryliu1995@gmail.com>
2024-04-18 10:19:47 -0400

    feat(aliases): update aliases

--- setup/dotfiles/zsh/scripts/set_fly_secret.sh
+++ setup/dotfiles/zsh/scripts/set_fly_secret.sh
@@ -1,13 +1,21 @@
 #!/bin/bash
 
-# Check if both arguments are provided
-if [ $# -ne 2 ]; then
-    echo "Usage: $0 <key_name> <key_value>"
+usage() {
+    echo "Usage: $0 <key_name> <key_value> [app_name]"
     exit 1
+}
+
+if [ $# -lt 2 ]; then
+    usage
 fi
 
 KEY_NAME="$1"
 KEY_VALUE="$2"
 
-# Set secret using fly command
-fly secrets set "$KEY_NAME=\"$KEY_VALUE\"" --stage
+if [ -n "$3" ]; then
+    USE_APP="--app $3"
+else
+    USE_APP=""
+fi
+
+fly secrets set "${KEY_NAME}=${KEY_VALUE}" $USE_APP --stage
