eb5f623d5605af32c7ae2e930b9e544c3b15c3fc
Harry Liu <harryliu1995@gmail.com>
2024-04-24 04:29:57 -0400

    feat(toronto-alerts): add debug lines to shell script

--- projects/toronto-alerts/toronto-alerts-flask/run.sh
+++ projects/toronto-alerts/toronto-alerts-flask/run.sh
@@ -1,12 +1,15 @@
+echo 'Setting environment variables.'
+
 {
   echo "GTA_UPDATE_ALERT_EMAIL=${GTA_UPDATE_ALERT_EMAIL}"
   echo "GTA_UPDATE_ALERT_EMAIL_PASSWORD=${GTA_UPDATE_ALERT_EMAIL_PASSWORD}"
   echo "GTA_UPDATE_ALERTS_DB=${GTA_UPDATE_ALERTS_DB}"
 } >> /etc/environment
-
 set -e
 
+echo 'Starting cron.'
 crontab /app/etc/crontab
 cron
 
+echo 'Starting server.'
 python3 -m flask run --host=0.0.0.0 --port=8080
