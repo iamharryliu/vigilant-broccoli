e0699dc1c9bfc42b10dd6600ad31cacde89f2d49
Harry Liu <harryliu1995@gmail.com>
2024-04-27 17:37:27 -0400

    feat(secrets-manager): secrets manager deployment config && secrets manager template

--- projects/secret-manager/flyio_secret_manager.py
+++ projects/secret-manager/flyio_secret_manager.py
@@ -1,4 +1,4 @@
-import os, json, subprocess
+import os, json, subprocess, threading, time
 
 CUSTOM_FLYIO_MAPPING = "custom-flyio-key-mapping.json"
 DEFAULT_FLYIO_MAPPING = "default-flyio-key-mapping.json"
@@ -7,7 +7,7 @@ DEFAULT_FLYIO_MAPPING = "default-flyio-key-mapping.json"
 def set_flyio_secrets(app):
     app_name = app["name"]
     secrets = app["password_key_values"]
-    print(f"FlyIO Secret Setting- Setting secrets for {app_name}.")
+    print(f"FlyIO Secrets Manager - Setting secrets for {app_name}.")
     for secret_name, secret_value in secrets.items():
         #     command = f"flyctl secrets unset --app {app_name} {secret_name} --stage"
         #     subprocess.run(command, shell=True,
@@ -19,22 +19,41 @@ def set_flyio_secrets(app):
         subprocess.run(
             command, shell=True, stdout=subprocess.DEVNULL, stderr=subprocess.STDOUT
         )
-    # command = f'flyctl deploy --app {app_name}'
-    # subprocess.run(command, shell=True)
-    # flyctl deploy --dockerfile Dockerfile --config config.toml
+    print(f"FlyIO Secret Manager - Finished setting secrets for {app_name}.")
+    if "working_directory" in app and "dockerfile" in app and "config" in app:
+        working_directory = app["working_directory"]
+        dockerfile = app["dockerfile"]
+        config = app["config"]
+        print(f"FlyIO Secrets Manager - Deploying {app_name}.")
+        command = f"cd {working_directory} && flyctl deploy --dockerfile {dockerfile} --config {config}"
+        subprocess.run(
+            command, shell=True, stdout=subprocess.DEVNULL, stderr=subprocess.STDOUT
+        )
+        print(f"FlyIO Secrets Manager - Finished deploying {app_name}.")
 
 
 def main():
-    print(f"FlyIO Secret Setting - Start.")
+    start_time = time.time()
+    print(f"FlyIO Secrets Manager - Start.")
     if os.path.exists(CUSTOM_FLYIO_MAPPING):
         with open(CUSTOM_FLYIO_MAPPING, "r") as file:
             apps = json.load(file)
     else:
         with open(DEFAULT_FLYIO_MAPPING, "r") as file:
             apps = json.load(file)
+
+    threads = []
     for app in apps:
-        set_flyio_secrets(app)
-    print(f"FlyIO Secret Setting - Finish.")
+        thread = threading.Thread(target=set_flyio_secrets, args=(app,))
+        thread.start()
+        threads.append(thread)
+
+    for thread in threads:
+        thread.join()
+
+    end_time = time.time()
+    elapsed_time = end_time - start_time
+    print(f"FlyIO Secrets Manager - Finished in {elapsed_time}s.")
 
 
 if __name__ == "__main__":
