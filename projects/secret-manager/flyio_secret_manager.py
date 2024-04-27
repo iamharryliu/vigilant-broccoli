1591441857f74733202ac8a7d8e0d1b73263ec83
Harry Liu <harryliu1995@gmail.com>
2024-04-27 15:59:25 -0400

    feat(secrets-manager): update logging

--- projects/secret-manager/flyio_secret_manager.py
+++ projects/secret-manager/flyio_secret_manager.py
@@ -4,29 +4,38 @@ CUSTOM_FLYIO_MAPPING = "custom-flyio-key-mapping.json"
 DEFAULT_FLYIO_MAPPING = "default-flyio-key-mapping.json"
 
 
-def set_flyio_secrets(app_name, secrets):
+def set_flyio_secrets(app):
+    app_name = app["name"]
+    secrets = app["password_key_values"]
+    print(f"FlyIO Secret Setting- Setting secrets for {app_name}.")
     for secret_name, secret_value in secrets.items():
-        command = f"flyctl secrets unset --app {app_name} {secret_name} --stage"
-        subprocess.run(command, shell=True)
+        #     command = f"flyctl secrets unset --app {app_name} {secret_name} --stage"
+        #     subprocess.run(command, shell=True,
+        # stdout=subprocess.DEVNULL,
+        # stderr=subprocess.STDOUT)
         command = (
             f"flyctl secrets set --app {app_name} {secret_name}={secret_value} --stage"
         )
-        subprocess.run(command, shell=True)
+        subprocess.run(
+            command, shell=True, stdout=subprocess.DEVNULL, stderr=subprocess.STDOUT
+        )
     # command = f'flyctl deploy --app {app_name}'
     # subprocess.run(command, shell=True)
+    # flyctl deploy --dockerfile Dockerfile --config config.toml
 
 
-if os.path.exists(CUSTOM_FLYIO_MAPPING):
-    with open(CUSTOM_FLYIO_MAPPING, "r") as file:
-        apps = json.load(file)
-else:
-    with open(DEFAULT_FLYIO_MAPPING, "r") as file:
-        apps = json.load(file)
+def main():
+    print(f"FlyIO Secret Setting - Start.")
+    if os.path.exists(CUSTOM_FLYIO_MAPPING):
+        with open(CUSTOM_FLYIO_MAPPING, "r") as file:
+            apps = json.load(file)
+    else:
+        with open(DEFAULT_FLYIO_MAPPING, "r") as file:
+            apps = json.load(file)
+    for app in apps:
+        set_flyio_secrets(app)
+    print(f"FlyIO Secret Setting - Finish.")
 
 
-for app in apps:
-    app_name = app["name"]
-    secrets = app["password_key_values"]
-    set_flyio_secrets(app_name, secrets)
-
-print("Secrets set successfully for all apps!")
+if __name__ == "__main__":
+    main()
