55b13e98585e7589f6b5a88f362051f3dec32020
Harry Liu <harryliu1995@gmail.com>
2024-05-01 12:00:45 -0400

    feat(aliases): update aliases

--- setup/dotfiles/zsh/aliases/quick_link_aliases.sh
+++ setup/dotfiles/zsh/aliases/quick_link_aliases.sh
@@ -1,20 +1,25 @@
-# Google Suite
-alias google="~/shell-scripts/google_search.sh"
-alias gmail='open -a "Google Chrome" "https://gmail.com/"'
-alias youtube="~/shell-scripts/youtube_search.sh"
-alias gphotos='open -a "Google Chrome" "https://photos.google.com/?pli=1"'
-alias gcalendar='open -a "Google Chrome" "https://calendar.google.com/calendar/u/0/r"'
-alias gtranslate='open -a "Google Chrome" "https://translate.google.com/"'
-alias gstorage='open -a "Google Chrome" "https://one.google.com/storage"'
-
 # General Links
-alias openudemy='open -a "Google Chrome" "https://www.udemy.com/"'
-alias openamazon='open -a "Google Chrome" "https://www.amazon.ca/"'
-alias openchat='open -a "Google Chrome" "https://chat.openai.com/"'
+alias google="~/shell-scripts/google_search.sh"
+alias youtube="~/shell-scripts/youtube_search.sh"
+alias chatgpt='open -a "Google Chrome" "https://chat.openai.com/"'
 alias amazon="~/shell-scripts/amazon_search.sh"
 alias pinterest="~/shell-scripts/pinterest_search.sh"
+alias gtranslate='open -a "Google Chrome" "https://translate.google.com/"'
 
-# Language Learning
+# Online Services
+# Google Services
+alias gmail='open -a "Google Chrome" "https://gmail.com/"'
+alias gphotos='open -a "Google Chrome" "https://photos.google.com/?pli=1"'
+alias gcalendar='open -a "Google Chrome" "https://calendar.google.com/calendar/u/0/r"'
+alias gstorage='open -a "Google Chrome" "https://one.google.com/storage"'
+# Apple
+alias findmy='open -a "Google Chrome" "https://www.icloud.com/find/"'
+alias applestorage='open -a "Google Chrome" "https://www.icloud.com/storage/"'
+# Storage
+alias checkstorage='applestorage && gstorage'
+
+# Learning
+alias udemy='open -a "Google Chrome" "https://www.udemy.com/"'
 alias memrise='open -a "Google Chrome" "https://app.memrise.com/dashboard"'
 alias communitymemrise='open -a "Google Chrome" "https://community-courses.memrise.com/dashboard"'
 alias udemyspanish='open -a "Google Chrome" "https://www.udemy.com/course/3-minute-spanish-course-5/learn"'
@@ -26,28 +31,25 @@ alias openresume='open -a "Google Chrome" "https://docs.google.com/document/d/1s
 alias openlinkedin='open -a "Google Chrome" "https://www.linkedin.com/jobs/"'
 alias jobhunt='openresume && openlinkedin'
 
-# Bill Pay
+# Finance
 alias openTD='open -a "Google Chrome" "https://authentication.td.com/uap-ui/?consumer=easyweb&locale=en_CA#/uap/login"'
+# Investment
+alias questtrade='open -a "Google Chrome" "https://login.questrade.com/account/login"'
+# Credit Accounts
 alias openNeo='open -a "Google Chrome" "https://member.neofinancial.com/login"'
 alias openMBNA='open -a "Google Chrome" "https://www.mbna.ca/en"'
 alias OpenCTTriangle='open -a "Google Chrome" "https://www.ctfs.com/content/dash/en/public/login.html"'
 alias openHomeDepot='open -a "Google Chrome" "https://citiretailservices.citibankonline.com/RSnextgen/svc/launch/index.action?siteId=CACN_HOMEDEPOT#signon"'
-alias openHomeDepot='open -a "Google Chrome" "https://www.homedepot.ca/en/home/credit-services.html"'
-alias paybill='openTD && openNeo && openMBNA && OpenCTTriangle && openHomeDepot'
-
-# Billing
+alias billpay='openTD && openNeo && openMBNA && OpenCTTriangle && openHomeDepot'
+# Paid Services
+alias freedomusage='open -a "Google Chrome" "https://myaccount.freedommobile.ca/usage"'
+alias presto='open -a "Google Chrome" "https://www.prestocard.ca/en/account-dashboard"'
+alias checkpaidservices='freedomusage && preso'
+# Dev Billing
 alias openFlyBilling='open -a "Google Chrome" "https://fly.io/dashboard/personal/billing"'
 alias openOpenAIBilling='open -a "Google Chrome" "https://platform.openai.com/account/billing/overview"'
 alias openAWSBilling='open -a "Google Chrome" "https://us-east-1.console.aws.amazon.com/costmanagement/home?region=us-east-1#/home"'
 alias checkDevBilling='openFlyBilling && openOpenAIBilling && openAWSBilling'
-
-# Health Checks
-alias freedomusage='open -a "Google Chrome" "https://myaccount.freedommobile.ca/usage"'
-alias presto='open -a "Google Chrome" "https://www.prestocard.ca/en/account-dashboard"'
-alias checkpaidservices='freedomusage && preso'
-
-alias applestorage='open -a "Google Chrome" "https://www.icloud.com/storage/"'
-alias checkstorage='applestorage && gstorage'
 
 # Iceskating
 alias heronparkleisureskateschedule='open -a "Google Chrome" "https://www.toronto.ca/data/parks/prd/skating/dropin/leisure/index.html#loc_633"'
@@ -55,13 +57,6 @@ alias centennialskateschedule='open -a "Google Chrome" "https://www.toronto.ca/d
 alias scarboroughvillageskateschedule='open -a "Google Chrome" "https://www.toronto.ca/data/parks/prd/skating/dropin/leisure/index.html#loc_743"'
 alias checkskatingtimes='heronparkleisureskateschedule && centennialskateschedule && scarboroughvillageskateschedule'
 
-# Utility
-alias findmy='open -a "Google Chrome" "https://www.icloud.com/find/"'
-
-# Git
-alias gitme='open -a "Google Chrome" "https://github.com/iamharryliu"'
-alias gitvb='open -a "Google Chrome" "https://github.com/iamharryliu/vigilant-broccoli"'
-alias gitactions='open -a "Google Chrome" "https://github.com/iamharryliu/vigilant-broccoli/actions"'
 
 # Toronto Alerts
 alias torontoalerts='open -a "Google Chrome" "https://torontoalerts.com/"'
--- setup/dotfiles/zsh/aliases/vigilant-broccoli_aliases.sh
+++ setup/dotfiles/zsh/aliases/vigilant-broccoli_aliases.sh
@@ -1,17 +1,25 @@
-
+# Github
+alias gitme='open -a "Google Chrome" "https://github.com/iamharryliu"'
+alias pushgitprofile="cd ~/iamharryliu && gpull && git add . && gc docs 'update github profile readme' && gpush"
 # vigilant-broccoli
+alias gitvb='open -a "Google Chrome" "https://github.com/iamharryliu/vigilant-broccoli"'
+alias gitactions='open -a "Google Chrome" "https://github.com/iamharryliu/vigilant-broccoli/actions"'
 alias cdvb="cd ~/$REPO_NAME/"
+alias pushreadme="cdvb && git add README.md && gc docs readme 'update root README.md' && gpush"
 alias pushnotes="cdvb && git add notes/**/*.md && gc docs 'update md notes' && gpush"
 alias pushtodo="cdvb && git add TODO.md && gc docs todo 'update TODO.md' && gpush"
-alias pushreadme="cdvb && git add README.md && gc docs readme 'update root README.md' && gpush"
-alias pushaliases="cdvb && git add setup/dotfiles/zsh/aliases/ setup/dotfiles/zsh/scripts/ && gc feat aliases 'update aliases' && gpush"
-alias pushdotfiles="cdvb && git add setup/dotfiles/* && gc docs 'update dotfiles' && gpush"
-alias pushactions="cdvb && git add .github/workflows && gc build github-actions 'update actions' && gpush"
-alias pushprogress="cdvb && git add snippets/progress.md && gc docs progress 'update progress.md' && gpush"
 alias pushsnippets="cdvb && git add snippets/ && gc docs snippets 'update snippets' && gpush"
-# MacOS Homebrew
+alias pushprogress="cdvb && git add snippets/progress.md && gc docs progress 'update progress.md' && gpush"
+# Actions
+alias pushactions="cdvb && git add .github/workflows && gc build github-actions 'update actions' && gpush"
+# Setup
+# Dotfiles
+alias pushdotfiles="cdvb && git add setup/dotfiles/* && gc docs 'update dotfiles' && gpush"
+alias pushaliases="cdvb && git add setup/dotfiles/zsh/aliases/ setup/dotfiles/zsh/scripts/ && gc feat aliases 'update aliases' && gpush"
+# Homebrew
 alias brewdump="cd vb && rm setup/mac/Brewfile && brew bundle dump --file=setup/mac/Brewfile"
 alias pushbrew="cdvb && git add setup/mac/Brewfile && gc feat brew 'update brew' && gpush"
+# Projects
 # Nx
 alias cdnx="cd ~/$REPO_NAME/projects/nx-workspace/"
 # harryliu.design
@@ -20,22 +28,16 @@ alias deploy-harryliu-design="cdnx && nx manual-deploy personal-website-frontend
 alias pushpagecontent="cdnx && git add apps/ui/personal-website-frontend/src/assets/site-content/ && gc feat personal-website 'update site content' && gpush"
 alias pushresume="cdnx && git add apps/ui/personal-website-frontend/src/assets/HarryLiu-Resume.pdf && gc docs resume 'update resume' && gpush"
 alias pushblog="cdnx && git add apps/ui/personal-website-frontend/src/assets/blogs && gc feat blog 'update blog' && gpush"
-
-# Github
-alias pushgitreadme="cd ~/iamharryliu && gpull && git add . && gc docs 'update github profile readme' && gpush"
-
-# Journal
-alias pushJournal="cd ~/journal && git add . && gc docs 'update journal' && gpush"
-
 # Grind 75
 alias grind75="python -m unittest discover -s ~/$REPO_NAME/projects/grind-75"
-
 # Toronto Alerts
 alias cdtorontoalerts="cdvb && cd projects/toronto-alerts"
-
 # DJ Stuff
 alias pushdj='cdvb && git subtree push --prefix=scripts/python/dj-scripts/spotify-to-mp3 git@github.com:iamharryliu/spotify-to-mp3.git main'
 alias dldjmusic="cd ~/$REPO_NAME/scripts/python/dj-scripts/spotify-to-mp3 && source venv/bin/activate && python download_music.py"
+
+# Journal
+alias pushJournal="cd ~/journal && git add . && gc docs 'update journal' && gpush"
 
 # Other
 alias sshpi="ssh hliu@192.168.1.104"
