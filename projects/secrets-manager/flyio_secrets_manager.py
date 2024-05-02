f5fbc72fc7f5aa8de29143d3d4960b905322dcb6
Harry Liu <harryliu1995@gmail.com>
2024-05-02 05:58:04 -0400

    refactor(secrets-manager): refactor flyio secrets manager

--- projects/secrets-manager/flyio_secrets_manager.py
+++ projects/secrets-manager/flyio_secrets_manager.py
@@ -1,18 +1,21 @@
 import os, json, subprocess, threading, time
 
-CUSTOM_FLYIO_MAPPING = "custom-flyio-key-mapping.json"
-DEFAULT_FLYIO_MAPPING = "default-flyio-key-mapping.json"
+
+class FLYIO_KEY_MAPPING:
+    CUSTOM_FLYIO_MAPPING = "custom-flyio-key-mapping.json"
+    DEFAULT_FLYIO_MAPPING = "default-flyio-key-mapping.json"
 
 
 class FlyIOSecretManager:
     @staticmethod
     def deploy_apps_using_mapping():
-        if os.path.exists(CUSTOM_FLYIO_MAPPING):
-            with open(CUSTOM_FLYIO_MAPPING, "r") as file:
-                apps = json.load(file)
+        if os.path.exists(FLYIO_KEY_MAPPING.CUSTOM_FLYIO_MAPPING):
+            MAPPING = FLYIO_KEY_MAPPING.CUSTOM_FLYIO_MAPPING
         else:
-            with open(DEFAULT_FLYIO_MAPPING, "r") as file:
-                apps = json.load(file)
+            MAPPING = FLYIO_KEY_MAPPING.DEFAULT_FLYIO_MAPPING
+
+        with open(MAPPING, "r") as file:
+            apps = json.load(file)
 
         threads = []
         for app in apps:
@@ -36,10 +39,6 @@ class FlyIOSecretManager:
         secrets = app["password_key_values"]
         print(f"FlyIO Secrets Manager - Setting secrets for {app_name}.")
         for secret_name, secret_value in secrets.items():
-            #     command = f"flyctl secrets unset --app {app_name} {secret_name} --stage"
-            #     subprocess.run(command, shell=True,
-            # stdout=subprocess.DEVNULL,
-            # stderr=subprocess.STDOUT)
             command = f"flyctl secrets set --app {app_name} {secret_name}={secret_value} --stage"
             subprocess.run(
                 command, shell=True, stdout=subprocess.DEVNULL, stderr=subprocess.STDOUT
@@ -48,8 +47,8 @@ class FlyIOSecretManager:
 
     @staticmethod
     def deploy_app(app):
-        app_name = app["name"]
         if "working_directory" in app and "dockerfile" in app and "config" in app:
+            app_name = app["name"]
             working_directory = app["working_directory"]
             dockerfile = app["dockerfile"]
             config = app["config"]
