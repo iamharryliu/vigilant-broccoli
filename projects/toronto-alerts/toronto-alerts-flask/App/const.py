13a6f6ccc3bff1814afe5f4109679a9dc2dbabf7
Harry Liu <harryliu1995@gmail.com>
2024-05-07 08:24:40 -0400

    test(toronto-alerts): implement pytest

--- projects/toronto-alerts/toronto-alerts-flask/App/const.py
+++ projects/toronto-alerts/toronto-alerts-flask/App/const.py
@@ -1,3 +1,19 @@
+class ENVIRONMENT_TYPE:
+    TEST = "TEST"
+    DIT = "DIT"
+    SIT = "SIT"
+    PROD = "PROD"
+
+
+class HTTP_STATUS_CODE:
+    OKAY = 200
+    BAD_REQUEST = 400
+    UNAUTHORIZED_REQUEST = 401
+    FORBIDDEN_REQUEST = 403
+    NOT_FOUND_ERROR = 404
+    INTERNAL_SERVER_ERROR = 500
+
+
 class ENDPOINT:
     INDEX = "/"
     SUBSCRIBE = "/subscribe"
