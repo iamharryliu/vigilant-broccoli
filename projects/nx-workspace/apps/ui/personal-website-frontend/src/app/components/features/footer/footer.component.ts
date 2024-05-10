9f69bd78f92aca7cb23096340ccc0a79a7ffcef2
Harry Liu <harryliu1995@gmail.com>
2024-05-10 15:28:48 -0400

    style(harryliu.design): create footer component and refactor template

---
+++ projects/nx-workspace/apps/ui/personal-website-frontend/src/app/components/features/footer/footer.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  standalone: true,
+  selector: 'app-footer',
+  templateUrl: './footer.component.html',
+})
+export class FooterComponent {}
--- projects/nx-workspace/apps/ui/personal-website-frontend/src/app/components/layouts/general/genreral-layout.component.ts
+++ projects/nx-workspace/apps/ui/personal-website-frontend/src/app/components/layouts/general/genreral-layout.component.ts
@@ -3,12 +3,13 @@ import { Component, Input } from '@angular/core';
 import { NavbarComponent } from '../../features/navbar/navbar.component';
 import { LINKS } from '../../../core/consts/routes.const';
 import { RouterModule } from '@angular/router';
+import { FooterComponent } from '../../features/footer/footer.component';
 
 @Component({
   standalone: true,
   selector: 'app-general-layout',
   templateUrl: './general-layout.component.html',
-  imports: [CommonModule, NavbarComponent, RouterModule],
+  imports: [CommonModule, NavbarComponent, FooterComponent, RouterModule],
   styles: [
     '#profile-picture {width: 120px; height: 120px; border-radius: 50%;}',
   ],
