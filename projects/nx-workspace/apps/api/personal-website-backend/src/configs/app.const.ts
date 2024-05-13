4f5895ed1c996088bdd2c045f67a7f1994442463
Harry Liu <harryliu1995@gmail.com>
2024-05-12 22:29:24 -0400

    fix(harryliu-design): test cors

--- projects/nx-workspace/apps/api/personal-website-backend/src/configs/app.const.ts
+++ projects/nx-workspace/apps/api/personal-website-backend/src/configs/app.const.ts
@@ -3,10 +3,9 @@ export const HOST = process.env.HOST || LOCAL_HOST_IP_ADDRESS;
 export const IS_DEV_ENV = HOST === LOCAL_HOST_IP_ADDRESS;
 export const PORT = process.env.PORT || 3000;
 export const ALLOWED_ORIGINS = [
-  'https://harryliu.design',
+  'https://harryliu.designasdf',
   'https://torontoalerts.com',
   'https://cloud8skate.com',
-  'https://cloud8skate.com/',
 ];
 export const CORS_OPTIONS = {
   origin: ALLOWED_ORIGINS,
