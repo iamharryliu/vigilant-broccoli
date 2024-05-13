3603168126dc527999a3e3b16b70406caad1cd05
Harry Liu <harryliu1995@gmail.com>
2024-05-12 23:59:51 -0400

    feat(cloud-8-skate-angular): add recaptcha interceptor

--- projects/nx-workspace/apps/ui/cloud-8-skate-angular/src/app/app.config.ts
+++ projects/nx-workspace/apps/ui/cloud-8-skate-angular/src/app/app.config.ts
@@ -2,16 +2,23 @@ import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { ROUTES } from './core/consts/routes.const';
 import { HTTP_INTERCEPTORS, provideHttpClient } from '@angular/common/http';
-import { CredentialsInterceptorService } from 'general-components';
+import {
+  CredentialsInterceptorService,
+  RecaptchaInterceptor,
+} from 'general-components';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(ROUTES),
     provideHttpClient(),
-
     {
       provide: HTTP_INTERCEPTORS,
       useClass: CredentialsInterceptorService,
+      multi: true,
+    },
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: RecaptchaInterceptor,
       multi: true,
     },
   ],
