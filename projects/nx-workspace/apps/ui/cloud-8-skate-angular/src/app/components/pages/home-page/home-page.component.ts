8739a4af6cf2dac643a47fe5f6e4111258eec802
Harry Liu <harryliu1995@gmail.com>
2024-05-15 14:12:14 -0400

    feat(cloud-8-skate-ui): add markdown content to home page

--- projects/nx-workspace/apps/ui/cloud-8-skate-angular/src/app/components/pages/home-page/home-page.component.ts
+++ projects/nx-workspace/apps/ui/cloud-8-skate-angular/src/app/components/pages/home-page/home-page.component.ts
@@ -2,13 +2,19 @@ import { Component } from '@angular/core';
 import { EXTERNAL_LINKS } from '../../../core/consts/routes.const';
 import { ContactSectionComponent } from '../../features/contact-section/contact-section.component';
 import { GeneralLayoutComponent } from '../../layouts/general-layout.component';
+import { MarkdownPageComponent } from 'general-components';
 
 @Component({
   standalone: true,
   selector: 'app-home-page',
   templateUrl: './home-page.component.html',
-  imports: [GeneralLayoutComponent, ContactSectionComponent],
+  imports: [
+    GeneralLayoutComponent,
+    ContactSectionComponent,
+    MarkdownPageComponent,
+  ],
 })
 export class HomePageComponent {
   EXTERNAL_LINKS = EXTERNAL_LINKS;
+  contentFilepath = 'assets/site-content/about.md';
 }
---
+++ projects/nx-workspace/apps/ui/cloud-8-skate-angular/src/assets/site-content/about.md
@@ -0,0 +1,3 @@
+# Cloud8Skate
+
+Hello we are Cloud8Skate, a Toronto based skating collective who's sole purpose is to skate our hearts out and have fun. We can usually be found goofing off at the <a href="https://maps.app.goo.gl/Aqu6WuqkiFAagZEK8" target="_blank">Bentway</a> or <a href="https://maps.app.goo.gl/BEv617tQgPUvRZGW8" target="_blank">College Park</a>.
