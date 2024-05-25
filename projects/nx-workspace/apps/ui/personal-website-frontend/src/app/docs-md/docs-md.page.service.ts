552ee200364ba274c07e2434d3ca487fff2e5d0b
Harry Liu <harryliu1995@gmail.com>
2024-05-25 10:07:39 -0400

    refactor(docs-md): use default const

--- projects/nx-workspace/apps/ui/personal-website-frontend/src/app/docs-md/docs-md.page.service.ts
+++ projects/nx-workspace/apps/ui/personal-website-frontend/src/app/docs-md/docs-md.page.service.ts
@@ -1,11 +1,13 @@
 import { Injectable } from '@angular/core';
 
+const DEFAULT_MD_FILE = 'assets/docs-md.md';
+
 @Injectable({
   providedIn: 'root',
 })
 export class DocsMdPageService {
+  selectedFile = DEFAULT_MD_FILE;
   isFileSelected = true;
-  selectedFile? = 'assets/docs-md.md';
 
   selectFile(filepath: string) {
     this.selectedFile = `assets/md-library/notes/${filepath}`;
@@ -13,7 +15,7 @@ export class DocsMdPageService {
   }
 
   closeSelectedFile(): void {
-    this.selectedFile = undefined;
+    this.selectedFile = DEFAULT_MD_FILE;
     this.isFileSelected = false;
   }
 }
