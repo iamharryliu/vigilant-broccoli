047b470bf9028ad3aa1a993869772f04de7979a4
Harry Liu <harryliu1995@gmail.com>
2024-05-25 09:48:02 -0400

    test(grind75): correct implement queue using stacks

--- projects/grind-75/array/implement_queue_using_stacks.py
+++ projects/grind-75/array/implement_queue_using_stacks.py
@@ -1,20 +1,23 @@
 class MyQueue:
     def __init__(self):
-        self.q = []
+        self.stack1 = []
+        self.stack2 = []
 
     def push(self, x: int) -> None:
-        self.q.append(x)
+        while self.stack1:
+            self.stack2.append(self.stack1.pop())
+        self.stack1.append(x)
+        while self.stack2:
+            self.stack1.append(self.stack2.pop())
 
     def pop(self) -> int:
-        tmp = self.q[0]
-        self.q = self.q[1:]
-        return tmp
+        return self.stack1.pop()
 
     def peek(self) -> int:
-        return self.q[0]
+        return self.stack1[-1]
 
     def empty(self) -> bool:
-        return len(self.q) == 0
+        return not self.stack1
 
 
 # Your MyQueue object will be instantiated and called as such:
