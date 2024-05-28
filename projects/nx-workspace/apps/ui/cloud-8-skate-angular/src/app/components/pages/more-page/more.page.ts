ec85c54ca557aece2cc942bc261becb3a9d710fe
Harry Liu <harryliu1995@gmail.com>
2024-05-28 00:34:05 -0400

    refactor(cloud8skate): refactor general-layout-component to app.component level

--- projects/nx-workspace/apps/ui/cloud-8-skate-angular/src/app/components/pages/more-page/more.page.ts
+++ projects/nx-workspace/apps/ui/cloud-8-skate-angular/src/app/components/pages/more-page/more.page.ts
@@ -1,12 +1,11 @@
 import { Component } from '@angular/core';
-import { GeneralLayoutComponent } from '../../layouts/general-layout.component';
 import { MarkdownPageComponent } from 'general-components';
 
 @Component({
   standalone: true,
   selector: 'app-more-page',
   templateUrl: './more.page.html',
-  imports: [GeneralLayoutComponent, MarkdownPageComponent],
+  imports: [MarkdownPageComponent],
 })
 export class MorePageComponent {
   contentFilepath = 'assets/site-content/more.md';
