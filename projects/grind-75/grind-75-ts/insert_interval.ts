617669f957b1f2af8b93943b1273506ffd2eecbf
Harry Liu <harryliu1995@gmail.com>
2024-06-03 14:53:40 -0400

    feat(grind-75): ts insert invertal

---
+++ projects/grind-75/grind-75-ts/insert_interval.ts
@@ -0,0 +1,19 @@
+function insert(intervals: number[][], newInterval: number[]): number[][] {
+  let res: number[][] = [];
+
+  for (let i = 0; i < intervals.length; i++) {
+    const interval = intervals[i];
+    if (interval[1] < newInterval[0]) {
+      res.push(intervals[i]);
+    } else if (interval[0] > newInterval[1]) {
+      res.push(newInterval);
+      return res.concat(intervals.slice(i));
+    } else {
+      newInterval = [
+        Math.min(interval[0], newInterval[0]),
+        Math.max(interval[1], newInterval[1]),
+      ];
+    }
+  }
+  return res.concat(newInterval);
+}
