a0782bb8a2b292c469de074e3e214bd4d09db191
Harry Liu <harryliu1995@gmail.com>
2024-06-05 12:14:09 -0400

    chore(demo-apps): delete flask-demo-api because features have been fully migrated to flask-demo

--- projects/demo-apps/flask-demo-api/App/password.py
+++
@@ -1,9 +0,0 @@
-import bcrypt
-
-salt = bcrypt.gensalt()
-
-
-class PasswordService:
-    @staticmethod
-    def hash_password(password):
-        return bcrypt.hashpw(password.encode("utf-8"), salt)
--- projects/demo-apps/flask-demo-api/App/users/routes.py
+++
@@ -1,4 +0,0 @@
-from App.users.users_create_routes import blueprint as users_create_blueprint
-from App.users.users_update_routes import blueprint as users_update_blueprint
-from App.users.users_session_routes import blueprint as users_session_blueprint
-from App.users.users_follow_routes import blueprint as users_follow_blueprint
