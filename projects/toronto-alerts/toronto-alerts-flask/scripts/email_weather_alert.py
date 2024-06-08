8e899286e4def2125d6134815f0cffa46a204756
Harry Liu <harryliu1995@gmail.com>
2024-06-08 14:58:43 -0400

    fix(toronto-alerts): fix weather email alert

--- projects/toronto-alerts/toronto-alerts-flask/scripts/email_weather_alert.py
+++ projects/toronto-alerts/toronto-alerts-flask/scripts/email_weather_alert.py
@@ -9,21 +9,21 @@ from email_subscribers import get_users, email_users
 def main():
     users = get_users()
     weather_data = get_weather_data()
-    subject = f"Toronto Alerts - Weather - {weather_data['weather']['description']}, {weather_data['weather']['temp']}C°"
+    subject = f"Toronto Alerts - Weather - {weather_data['weather']['description']}, {weather_data['weather']['temp']}C"
     body = (
-        f"Today's weather is {weather_data['weather']['description']}, {weather_data['weather']['temp_min']}C° to {weather_data['weather']['temp_max']}C°.\n\n"
-        + f"Currently {weather_data['weather']['temp']}C°.\n"
-        + f"Feels like {weather_data['weather']['feels_like']}C°.\n\n"
+        f"Today's weather is {weather_data['weather']['description']}, {weather_data['weather']['temp_min']}C to {weather_data['weather']['temp_max']}C.\n\n"
+        + f"Currently {weather_data['weather']['temp']}C.\n"
+        + f"Feels like {weather_data['weather']['feels_like']}C.\n\n"
         + f"{weather_data['weather']['clouds']}% Cloudiness\n{weather_data['weather']['humidity']}% Humidity\n\n"
         + f"Sunrise at {weather_data['sun']['sunrise'].strftime('%-H:%M')}am\nSunset at {weather_data['sun']['sunset'].strftime('%-H:%M')}pm"
     )
 
     emails = [
         {
-            "email": user["email"],
+            "email": user.email,
             "message": {
                 "from": "Toronto Alerts",
-                "to": user["email"],
+                "to": user.email,
                 "subject": subject,
                 "body": body,
             },
