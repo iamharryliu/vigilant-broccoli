7aa73324ead4e84bffd83b904fe128fd42c7ca2b
Harry Liu <harryliu1995@gmail.com>
2024-07-01 14:37:13 -0400

    chore(react-demo): remove pages

--- projects/demo-apps/react-demo/src/App.tsx
+++ projects/demo-apps/react-demo/src/App.tsx
@@ -2,8 +2,6 @@ import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Layout from './layouts/GeneralPageLayout';
 import IndexPage from './pages/IndexPage';
-import ConditionalRenderPage from './pages/ConditionalRenderPage';
-import PropsPage from './pages/PropsPage';
 import HooksPage from './pages/Hooks/HooksPage';
 import TodoPage from './pages/Todo/TodoPage';
 import UseEffectDemo from './pages/Hooks/UseEffectDemo';
@@ -18,11 +16,6 @@ export default function App() {
         <Routes>
           <Route path="" element={<Layout />}>
             <Route index element={<IndexPage />} />
-            <Route
-              path="conditional-render"
-              element={<ConditionalRenderPage />}
-            />
-            <Route path="props" element={<PropsPage />} />
             <Route path="hooks" element={<HooksPage />} />
             <Route path="todo" element={<TodoPage />} />
             <Route path="hooks/useEffect" element={<UseEffectDemo />} />
--- projects/demo-apps/react-demo/src/components/NavbarComponent.tsx
+++ projects/demo-apps/react-demo/src/components/NavbarComponent.tsx
@@ -23,16 +23,6 @@ export default function NavbarComponent() {
         <div className="collapse navbar-collapse" id="navbarSupportedContent">
           <ul className="navbar-nav me-auto mb-2 mb-lg-0">
             <li className="nav-item">
-              <Link className="nav-link" to="/conditional-render">
-                Conditional Render
-              </Link>
-            </li>
-            <li className="nav-item">
-              <Link className="nav-link" to="/props">
-                Props
-              </Link>
-            </li>
-            <li className="nav-item">
               <Link className="nav-link" to="/hooks">
                 Hooks
               </Link>
