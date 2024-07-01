31f16c784eda823bc1b0eed1c3aebda95039c115
Harry Liu <harryliu1995@gmail.com>
2024-07-01 16:24:38 -0400

    fix(react-demo): fix navbar collapse

--- projects/demo-apps/react-demo/src/components/NavbarComponent.tsx
+++ projects/demo-apps/react-demo/src/components/NavbarComponent.tsx
@@ -1,8 +1,21 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import 'bootstrap/js/src/collapse.js';
+import { Collapse } from 'bootstrap';
+
+const COLLAPSABLE_NAVBAR_CONTENT_ID = 'navbarSupportedContent';
 
 export default function NavbarComponent() {
+  function collapse() {
+    const navbarCollapse = document.getElementById(
+      COLLAPSABLE_NAVBAR_CONTENT_ID,
+    );
+    if (navbarCollapse) {
+      const collapse = new Collapse(navbarCollapse);
+      collapse.hide();
+    }
+  }
+
   return (
     <nav className="navbar navbar-expand-lg bg-body-tertiary">
       <div className="container-fluid">
@@ -20,15 +33,18 @@ export default function NavbarComponent() {
         >
           <span className="navbar-toggler-icon"></span>
         </button>
-        <div className="collapse navbar-collapse" id="navbarSupportedContent">
+        <div
+          className="collapse navbar-collapse"
+          id={COLLAPSABLE_NAVBAR_CONTENT_ID}
+        >
           <ul className="navbar-nav me-auto mb-2 mb-lg-0">
             <li className="nav-item">
-              <Link className="nav-link" to="/hooks">
+              <Link className="nav-link" to="/hooks" onClick={collapse}>
                 Hooks
               </Link>
             </li>
             <li className="nav-item">
-              <Link className="nav-link" to="/todo">
+              <Link className="nav-link" to="/todo" onClick={collapse}>
                 Todo
               </Link>
             </li>
