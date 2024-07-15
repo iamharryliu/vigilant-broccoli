d6dd2d76a0cd0877aa21405d2cbb0079218187dd
Harry Liu <harryliu1995@gmail.com>
2024-07-15 23:32:14 +0200

    feat(setup): enhance setup scripts

--- setup/mac/setup_macos_preferences.sh
+++ setup/mac/setup_macos_preferences.sh
@@ -22,6 +22,8 @@ killall Dock
 defaults write -g com.apple.swipescrolldirection -bool false
 
 # Hot Corners
+defaults write com.apple.dock wvous-tl-corner -int 12
+defaults write com.apple.dock wvous-tl-modifier -int 0
 defaults write com.apple.dock wvous-tr-corner -int 2
 defaults write com.apple.dock wvous-tr-modifier -int 0
 defaults write com.apple.dock wvous-bl-corner -int 13
