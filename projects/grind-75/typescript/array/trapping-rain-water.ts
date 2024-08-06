7cbabbe78fff4f599b63b163a69fa90534faaf05
Harry Liu <harryliu1995@gmail.com>
2024-08-06 15:46:20 +0200

    feat(grind-75): trapping rainwater in ts

---
+++ projects/grind-75/typescript/array/trapping-rain-water.ts
@@ -0,0 +1,20 @@
+export function trap(height: number[]): number {
+    let res = 0
+    let l=0
+    let r=height.length-1
+    let lmax=height[l]
+    let rmax=height[r]
+    while(l<r){
+        if (lmax < rmax){
+            l+=1
+            lmax= Math.max(lmax,height[l])
+            res+=lmax-height[l]
+        }
+        else{
+            r-=1
+            rmax=Math.max(rmax,height[r])
+            res+=rmax-height[r]
+        }
+    }
+    return res
+};
