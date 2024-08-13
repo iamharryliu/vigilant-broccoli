d448ccae4c1b546df4e04822f0b2b40fbb989735
Harry Liu <harryliu1995@gmail.com>
2024-08-13 10:20:50 +0200

    feat: update valid_anagram

--- projects/grind-75/python/string_lc_questions/valid_anagram.py
+++ projects/grind-75/python/string_lc_questions/valid_anagram.py
@@ -1,22 +1,13 @@
 class Solution:
     @classmethod
     def isAnagram(self, s, t):
-        """
-        :type s: str
-        :type t: str
-        :rtype: bool
-        """
-        for c in set(s + t):
-            if s.count(c) != t.count(c):
-                return False
-        return True
-
-        # Hashmap
-        # dic1, dic2 = {}, {}
-        # # loop through strings and increment counter for characters
-        # for item in s:
-        #     dic1[item] = dic1.get(item, 0) + 1
-        # for item in t:
-        #     dic2[item] = dic2.get(item, 0) + 1
-        # # check that dics are the same
-        # return dic1 == dic2
+        if len(s) != len(t):
+            return False
+        count_s, count_t = {}, {}
+        # loop through strings and increment counter for characters
+        for item in s:
+            count_s[item] = count_s.get(item, 0) + 1
+        for item in t:
+            count_t[item] = count_t.get(item, 0) + 1
+        # check that dics are the same
+        return count_s == count_t
