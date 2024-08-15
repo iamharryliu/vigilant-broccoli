39046e4ec03110522ccefd82942c93d776fa5b89
Harry Liu <harryliu1995@gmail.com>
2024-08-15 15:25:06 +0200

    docs: update dotfiles

--- setup/dotfiles/zsh/.aliases.zsh
+++ setup/dotfiles/zsh/.aliases.zsh
@@ -1,8 +1,8 @@
 alias initsh='echo "~Shell initialized.~"; source ~/.zshrc'
 
 source ~/shell-aliases/dir_aliases.sh
-source ~/shell-aliases/quick_link_aliases.sh
 source ~/shell-aliases/git_aliases.sh
+source ~/shell-aliases/quick_link_aliases.sh
 source ~/shell-aliases/python_aliases.sh
 source ~/shell-aliases/devops_aliases.sh
 source ~/shell-aliases/vigilant-broccoli_aliases.sh
