587b62014190ffd96af0797338bccf72d2d75740
Harry Liu <harryliu1995@gmail.com>
2024-08-19 13:51:39 +0200

    feat(grind-75): Update testcase

--- projects/grind-75/python/test/test_k_closest_points_to_origin.py
+++ projects/grind-75/python/test/test_k_closest_points_to_origin.py
@@ -5,6 +5,6 @@ from heap.k_closest_points_to_origin import Solution
 class TestSolution(unittest.TestCase):
     def test(self):
         res = Solution.kClosest(points=[[1, 3], [-2, 2]], k=1)
-        self.assertEqual(res, [[-2, 2]])
+        self.assertCountEqual(res, [[3, 3], [-2, 4]])
         res = Solution.kClosest(points=[[3, 3], [5, -1], [-2, 4]], k=2)
         self.assertCountEqual(res, [[3, 3], [-2, 4]])
