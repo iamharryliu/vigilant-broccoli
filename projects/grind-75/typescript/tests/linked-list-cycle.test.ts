a401e175f938d10489f3e4c9a2046320cdcc5837
Harry Liu <harryliu1995@gmail.com>
2024-08-20 23:01:55 +0200

    fix(grind75): fix tc name

--- projects/grind-75/typescript/tests/linked-list-cycle.test.ts
+++ projects/grind-75/typescript/tests/linked-list-cycle.test.ts
@@ -1,7 +1,7 @@
 import { LinkedList } from '../linked-list/common';
 import hasCycle from '../linked-list/linked-list-cycle';
 
-test('twoSum', () => {
+test('hasCycle', () => {
   expect(hasCycle(new LinkedList([3, 2, 0, -4], 1).head)).toEqual(true);
   expect(hasCycle(new LinkedList([1, 2], 0).head)).toEqual(true);
   expect(hasCycle(new LinkedList([1], -1).head)).toEqual(false);
