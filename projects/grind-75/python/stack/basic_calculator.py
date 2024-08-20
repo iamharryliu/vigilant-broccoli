1ae4023eb2ecd8aa08ba1ca7feac00e43ed10edd
Harry Liu <harryliu1995@gmail.com>
2024-08-20 12:05:02 +0200

    feat(grind-75): Update Grind 75

--- projects/grind-75/python/stack/basic_calculator.py
+++ projects/grind-75/python/stack/basic_calculator.py
@@ -1,22 +1,26 @@
 class Solution:
     @classmethod
     def calculate(self, s: str) -> int:
-        stack = [1, 1]
+        stack = [1]
         res = 0
         i = 0
+        sign = 1
         while i < len(s):
             if s[i].isdigit():
                 num = 0
                 while i < len(s) and s[i].isdigit():
                     num = num * 10 + int(s[i])
                     i += 1
-                res += stack.pop() * num
-            else:
-                if s[i] in "+(":
-                    stack.append(stack[-1])
-                if s[i] == "-":
-                    stack.append(-stack[-1])
-                if s[i] == ")":
-                    stack.pop()
-                i += 1
+                res += sign * num
+                continue
+            elif s[i] == "+":
+                sign = stack[-1]
+            elif s[i] == "-":
+                sign = -stack[-1]
+            elif s[i] == "(":
+                stack.append(sign)
+            elif s[i] == ")":
+                stack.pop()
+            i += 1
+
         return res
