9d38f135281615f8dde06368ed804e099440d689
Harry Liu <harryliu1995@gmail.com>
2024-08-20 18:46:20 +0200

    feat(grind-75): Binary tree level order traversal in Typescript

--- projects/grind-75/typescript/tree/bt-level-order-traversal.ts
+++ projects/grind-75/typescript/tree/bt-level-order-traversal.ts
@@ -1,24 +1,23 @@
-import { TreeNode } from "./common"
+import { TreeNode } from './common';
 
 export default function levelOrder(root: TreeNode | null): number[][] {
-    
-    if(!root) return []
-    const q = [root]
-    const res = []
+  if (!root) return [];
+  const q = [root];
+  const res = [];
 
-    while (q.length){
-        res.push(q.map(node=>node.val))
-        const currLenQ = q.length
-        for(let i=0; i< currLenQ; i++){
-            const node = q.shift()!
-            if(node.left){
-                q.push(node.left)
-            }
-            if(node.right){
-                q.push(node.right)
-            }
-        }
+  while (q.length) {
+    res.push(q.map(node => node.val));
+    const currLenQ = q.length;
+    for (let i = 0; i < currLenQ; i++) {
+      const node = q.shift()!;
+      if (node.left) {
+        q.push(node.left);
+      }
+      if (node.right) {
+        q.push(node.right);
+      }
     }
+  }
 
-    return res
-};
+  return res;
+}
