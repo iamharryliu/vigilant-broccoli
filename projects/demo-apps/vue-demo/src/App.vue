954e16d823755992dcbe8da71d514427f0805578
Harry Liu <harryliu1995@gmail.com>
2024-08-28 13:54:26 +0200

    refactor(vue-demo):  Make navbar component.

--- projects/demo-apps/vue-demo/src/App.vue
+++ projects/demo-apps/vue-demo/src/App.vue
@@ -1,37 +1,9 @@
 <script setup lang="ts">
-import { RouterLink, RouterView } from 'vue-router'
-
-const COLLAPSABLE_NAVBAR_CONTENT = {
-  ID: 'navbarSupportedContent',
-  LINKS: ['todo']
-}
-
-const toggle = () => {}
-const collapse = () => {}
-
-const toTitleCase = (str: string) =>
-  str.replace(/\w\S*/g, (text) => text.charAt(0).toUpperCase() + text.substring(1).toLowerCase())
+import { RouterView } from 'vue-router'
+import Navbar from './components/Navbar.vue'
 </script>
 
 <template>
-  <header>
-    <nav class="navbar navbar-expand-lg bg-body-tertiary">
-      <div class="container-fluid">
-        <RouterLink class="navbar-brand" to="/">Home</RouterLink>
-        <button class="navbar-toggler" type="button" @click="toggle">
-          <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="collapse navbar-collapse" :id="COLLAPSABLE_NAVBAR_CONTENT.ID">
-          <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-            <li class="nav-item" v-for="link in COLLAPSABLE_NAVBAR_CONTENT.LINKS" :key="link">
-              <RouterLink class="nav-link" :to="link" @click="collapse">{{
-                toTitleCase(link)
-              }}</RouterLink>
-            </li>
-          </ul>
-        </div>
-      </div>
-    </nav>
-  </header>
+  <Navbar />
   <RouterView />
 </template>
