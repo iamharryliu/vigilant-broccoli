cc5479c280aaf228c62bb9e2cf4979ce9a76ae4a
Harry Liu <harryliu1995@gmail.com>
2024-08-30 08:52:08 +0200

    refactor(vue-demo): Refactor TodoView.vue.

--- projects/demo-apps/vue-demo/src/views/TodoView.vue
+++ projects/demo-apps/vue-demo/src/views/TodoView.vue
@@ -1,61 +1,12 @@
 <script setup lang="ts">
-import { ref, onMounted } from 'vue'
-import { useTodoStore } from '../store/todoStore.ts'
-import { storeToRefs } from 'pinia'
 import TodoForm from '../components/forms/TodoForm.vue'
-
-const todoStore = useTodoStore()
-const { todos } = storeToRefs(todoStore)
-const { fetchTodos, updateTodo, deleteTodo } = todoStore
-
-const editingId = ref<number | null>(null)
-const editingValue = ref<string | null>(null)
-
-onMounted(async () => {
-  await fetchTodos()
-})
-
-const handleEditClick = (todo: Todo) => {
-  editingId.value = todo.id
-  editingValue.value = todo.title
-}
-
-const handleTodoUpdate = () => {
-  if (!editingValue.value) {
-    deleteTodo(editingId.value!)
-    return
-  }
-  updateTodo({ id: editingId.value!, title: editingValue.value })
-  editingId.value = null
-  editingValue.value = ''
-}
-
+import TodoList from '../components/TodoList.vue'
 </script>
 
 <template>
   <main class="container">
     <h1>Todo</h1>
     <TodoForm />
-    <ul class="list-group">
-      <template v-if="todos.length">
-        <li class="list-group-item" v-for="todo in todos" :key="todo.id">
-          <input
-            v-if="editingId === todo.id"
-            type="text"
-            v-model="editingValue"
-            @blur="handleTodoUpdate"
-            @keyup.enter="handleTodoUpdate"
-          />
-          <span v-else @click="handleEditClick(todo)">
-            {{ todo.title }}
-          </span>
-          <div class="float-end">
-            <button class="btn btn-primary me-2" @click="handleEditClick(todo)">Edit</button>
-            <button class="btn btn-danger" @click="deleteTodo(todo.id)">Delete</button>
-          </div>
-        </li>
-      </template>
-      <p v-else class="text-center my-4">You currently have no todo items.</p>
-    </ul>
+    <TodoList />
   </main>
 </template>
