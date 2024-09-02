9f87b009a2fd3ad3cf9acfff4f2f593873f39c6e
Harry Liu <harryliu1995@gmail.com>
2024-09-02 20:23:29 +0200

    refactor(vue-demo): Refactor demo.

--- projects/demo-apps/vue-demo/src/config.ts
+++ projects/demo-apps/vue-demo/src/config.ts
@@ -1,7 +1,6 @@
-const TODO_API_URL = 'http://localhost:8080/todo'
-export const TODO_ENDPOINT = {
-  CREATE_TODO: `${TODO_API_URL}/createTodo`,
-  GET_TODOS: `${TODO_API_URL}/getTodos`,
-  UPDATE_TODO: `${TODO_API_URL}/updateTodo`,
-  DELETE_TODO: `${TODO_API_URL}/deleteTodo`
+export const TODO_API_URL = 'https://jsonplaceholder.typicode.com/posts'
+export const TODO_HEADERS = {
+  headers: {
+    'Content-Type': 'application/json'
+  }
 }
--- projects/demo-apps/vue-demo/src/store/todoStore.ts
+++ projects/demo-apps/vue-demo/src/store/todoStore.ts
@@ -1,7 +1,7 @@
 import { defineStore } from 'pinia'
 import { ref } from 'vue'
 import axios from 'axios'
-import { TODO_ENDPOINT } from '@/config'
+import { TODO_API_URL, TODO_HEADERS } from '@/config'
 
 export type Todo = {
   id: number
@@ -16,37 +16,53 @@ export const useTodoStore = defineStore('todo', () => {
       const response = await axios.get('https://jsonplaceholder.typicode.com/todos')
       todos.value = response.data.slice(0, 10)
     } catch (error) {
-      console.error('Error fetching todos:', error)
+      console.error('Error fetching todos: ', error)
     }
   }
 
   const createTodo = async (title: string) => {
     try {
       const response = await axios.post(
-        TODO_ENDPOINT.CREATE_TODO,
+        TODO_API_URL,
         {
           title
         },
-        {
-          headers: {
-            'Content-Type': 'application/json'
-          }
-        }
+        TODO_HEADERS
       )
-
-      const payload = response.data
+      const payload = {
+        ...response.data,
+        id:
+          todos.value.reduce((max, todo) => {
+            return todo.id > max ? todo.id : max
+          }, Number.NEGATIVE_INFINITY) + 1
+      }
       todos.value = [...todos.value, payload]
     } catch (error) {
       console.error('Error creating todo:', error)
     }
   }
 
-  const updateTodo = (updatedTodo: Todo) => {
-    todos.value = todos.value.map((todo) => (updatedTodo.id === todo.id ? updatedTodo : todo))
+  const updateTodo = async (updatedTodo: Todo) => {
+    try {
+      const response = await axios.put(
+        `${TODO_API_URL}/${updatedTodo.id}`,
+        updatedTodo,
+        TODO_HEADERS
+      )
+      const payload = response.data
+      todos.value = todos.value.map((todo) => (updatedTodo.id === todo.id ? payload : todo))
+    } catch (error) {
+      console.error('Error updating todo: ', error)
+    }
   }
 
-  const deleteTodo = (id: number) => {
-    todos.value = todos.value.filter((todo) => todo.id != id)
+  const deleteTodo = async (id: number) => {
+    try {
+      const response = await axios.delete(`${TODO_API_URL}/${id}`, TODO_HEADERS)
+      if (response.status === 200) todos.value = todos.value.filter((todo) => todo.id != id)
+    } catch (error) {
+      console.log('Error deleting todo: ', error)
+    }
   }
 
   return {
