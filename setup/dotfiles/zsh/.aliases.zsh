c4f3e0d5d29de298ed573afeb710aa4f2e64f195
Harry Liu <harryliu1995@gmail.com>
2024-09-13 09:06:39 +0200

    feat(aliases): Update aliases.

--- setup/dotfiles/zsh/.aliases.zsh
+++ setup/dotfiles/zsh/.aliases.zsh
@@ -7,6 +7,6 @@ source ~/shell-aliases/quick_link_aliases.sh
 source ~/shell-aliases/python_aliases.sh
 source ~/shell-aliases/devops_aliases.sh
 source ~/shell-aliases/vigilant-broccoli_aliases.sh
-if [ -f ~/shell-aliases/personal-aliases ]; then
-    source ~/shell-aliases/personal-aliases
+if [ -f ~/shell-aliases/personal-aliases.sh ]; then
+    source ~/shell-aliases/personal-aliases.sh
 fi
