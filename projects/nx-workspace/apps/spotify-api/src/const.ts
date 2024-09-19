0aad9eb0a8460aab583cc443d47434f3eed6af69
Harry Liu <harryliu1995@gmail.com>
2024-09-19 09:23:49 +0200

    fix(spotify-api): Env fixes

--- projects/nx-workspace/apps/spotify-api/src/const.ts
+++ projects/nx-workspace/apps/spotify-api/src/const.ts
@@ -1,9 +1,12 @@
-export const HOST = 'localhost';
+export const HOST = process.env.HOST || 'localhost';
 export const PORT = 8080;
 
 export const SPOTIFY_CLIENT_ID = process.env.SPOTIFY_CLIENT_ID;
 export const SPOTIFY_CLIENT_SECRET = process.env.SPOTIFY_CLIENT_SECRET;
-export const REDIRECT_URI = `http://${HOST}:${PORT}/callback`;
+export const REDIRECT_URI =
+  process.env.NODE_ENV === 'LOCAL'
+    ? `http://${HOST}:${PORT}/callback`
+    : 'https://spotify-playlist-getter.fly.dev/callback';
 
 const scopes = 'playlist-read-private';
 export const authorizeUrl = `https://accounts.spotify.com/authorize?response_type=code&client_id=${SPOTIFY_CLIENT_ID}&scope=${encodeURIComponent(
