0b2b7cb3fc2ca5a05c3a70c774a8deaf7bb156c5
Harry Liu <harryliu1995@gmail.com>
2024-09-20 09:31:20 +0200

    feat(aliases): Update aliases.

--- setup/dotfiles/zsh/aliases/quick_link_aliases.sh
+++ setup/dotfiles/zsh/aliases/quick_link_aliases.sh
@@ -1,3 +1,33 @@
+
+# Browsers
+alias chrome='open -a "Google Chrome"'
+alias firefox='open -a "Firefox"'
+
+# Utility
+alias google="~/shell-scripts/google_search.sh"
+alias gmaps='chrome "https://www.google.com/maps"'
+alias chatgpt='function _gptquery() { local query=$(echo "$*" | sed "s/ /%20/g"); open "https://chatgpt.com/?q=${query}"; }; _gptquery'
+alias youtube="~/shell-scripts/youtube_search.sh"
+alias gtranslate='chrome "https://translate.google.com/"'
+alias pinterest="~/shell-scripts/pinterest_search.sh"
+alias amazon="~/shell-scripts/amazon_search.sh"
+alias karmanow='chrome "https://www.karmanow.com/my-items"'
+alias numi='open -a "Numi"'
+alias speedtest="chrome 'https://www.speedtest.net/'"
+alias pwgen="chrome 'https://passwordsgenerator.net/'"
+alias obsidian="open -a 'Obsidian'"
+
+# Services
+alias gmail='chrome "https://gmail.com/"'
+alias gcalendar='chrome "https://calendar.google.com/calendar/u/0/r"'
+alias gdrive='chrome "https://drive.google.com/drive/u/0/my-drive"'
+alias gcontacts='chrome "https://contacts.google.com/"'
+alias findmy='chrome "https://www.icloud.com/find/"'
+alias gphotos='chrome "https://photos.google.com/?pli=1"'
+alias gstorage='chrome "https://one.google.com/storage/management"'
+alias applestorage='chrome "https://www.icloud.com/storage/"'
+alias checkstorage='applestorage && gstorage'
+
 # Communication
 alias openMessages="open -a 'Messages'"
 alias igMessenger='chrome "https://www.instagram.com/direct/inbox/"'
@@ -5,37 +35,6 @@ alias fbMessenger="chrome 'https://www.messenger.com/'"
 alias openWhatsApp="open -a 'WhatsApp'"
 alias openSlack="open -a 'Slack'"
 alias openSlackBrowser="chrome 'https://app.slack.com/client/'"
-
-# Online Services
-## Google Workspace
-alias gmail='chrome "https://gmail.com/"'
-alias gcalendar='chrome "https://calendar.google.com/calendar/u/0/r"'
-alias gdrive='chrome "https://drive.google.com/drive/u/0/my-drive"'
-alias gcontacts='chrome "https://contacts.google.com/"'
-## Apple
-alias findmy='chrome "https://www.icloud.com/find/"'
-## Storage
-alias gphotos='chrome "https://photos.google.com/?pli=1"'
-alias gstorage='chrome "https://one.google.com/storage/management"'
-alias applestorage='chrome "https://www.icloud.com/storage/"'
-alias checkstorage='applestorage && gstorage'
-
-# Utility
-## General
-alias chrome='open -a "Google Chrome"'
-alias google="~/shell-scripts/google_search.sh"
-alias youtube="~/shell-scripts/youtube_search.sh"
-alias gmaps='chrome "https://www.google.com/maps"'
-alias gtranslate='chrome "https://translate.google.com/"'
-alias pinterest="~/shell-scripts/pinterest_search.sh"
-alias chatgpt='function _gptquery() { local query=$(echo "$*" | sed "s/ /%20/g"); open "https://chatgpt.com/?q=${query}"; }; _gptquery'
-
-
-alias numi='open -a "Numi"'
-alias speedtest="chrome 'https://www.speedtest.net/'"
-alias pwgen="chrome 'https://passwordsgenerator.net/'"
-## Notes
-alias openobsidian="open -a 'Obsidian'"
 
 # Learning
 alias udemy='chrome "https://www.udemy.com/"'
@@ -45,10 +44,6 @@ alias udemyspanish='chrome "https://www.udemy.com/course/3-minute-spanish-course
 alias udemyfrench='chrome "https://www.udemy.com/course/3-minute-french-course-8/learn"'
 alias languagepread='cdvb && code notes/language-learning/language-spread.md'
 alias languagelearn="languagepread && gtranslate && communitymemrise && udemyspanish && udemyfrench && memrise"
-
-# Consumerism
-alias amazon="~/shell-scripts/amazon_search.sh"
-alias karmanow='chrome "https://www.karmanow.com/my-items"'
 
 # Career
 alias openresume='chrome "https://docs.google.com/document/d/1s6Wy8i4zU85o19qyXKhdpH4jdTP36QDPUgZdV7E6-QU/edit#heading=h.uzt44hq0695d"'
