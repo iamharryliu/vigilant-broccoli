8c6ba48a3197d0b406232f163e0e92cfe954f8f9
Harry Liu <harryliu1995@gmail.com>
2024-09-26 15:10:50 +0200

    feat(aliases): Update aliases.

--- setup/dotfiles/zsh/aliases/devops/aws_aliases.sh
+++ setup/dotfiles/zsh/aliases/devops/aws_aliases.sh
@@ -1,6 +1,6 @@
 alias awsauto="aws --cli-auto-prompt"
 alias startec2="aws ec2 start-instances --instance-ids $ec2_instance"
 alias stopec2="aws ec2 stop-instances --instance-ids $ec2_instance"
-alias sshec2="ssh -i ~/certs/ec2-instance.pem $ec2_instance_username_ip_address"
+alias sshawsec2="ssh -i ~/certs/ec2-instance.pem $ec2_instance_username_ip_address"
 # Billing
 alias openAWSBilling='chrome "https://us-east-1.console.aws.amazon.com/costmanagement/home?region=us-east-1#/home"'
--- setup/dotfiles/zsh/aliases/devops/google_aliases.sh
+++ setup/dotfiles/zsh/aliases/devops/google_aliases.sh
@@ -1,6 +1,9 @@
 # Google Cloud
 alias gconsole='chrome "https://console.cloud.google.com/"'
 alias gce='chrome "https://console.cloud.google.com/compute/"'
+sshgceec2() {
+    ssh -i ~/.ssh/google-ec2 "$GOOGLE_EC2_HOST_ADDRESS"
+}
 
 # Analytics
 alias ganalytics='chrome "https://analytics.google.com/analytics/"'
