f71b9b2d31f7072f471dceb7373ced8c3747b369
Harry Liu <harryliu1995@gmail.com>
2024-09-26 14:53:16 +0200

    feat(aliases): Update aliases.

--- setup/dotfiles/zsh/aliases/devops/google_aliases.sh
+++ setup/dotfiles/zsh/aliases/devops/google_aliases.sh
@@ -1,7 +1,12 @@
+# Google Cloud
+alias gconsole='chrome "https://console.cloud.google.com/"'
+alias gce='chrome "https://console.cloud.google.com/compute/"'
+
 # Analytics
 alias ganalytics='chrome "https://analytics.google.com/analytics/"'
 alias gcaptcha='chrome "https://www.google.com/recaptcha/admin/"'
-# Google Workspace
+
+# Google Workspace APIs
 alias gam="~/bin/gam/gam"
 alias gyb="~/bin/gyb/gyb"
 alias advgam="~/bin/gamadv-xtd3/gam"
