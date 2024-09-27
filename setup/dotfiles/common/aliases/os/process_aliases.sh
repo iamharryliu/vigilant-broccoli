94c48fc47d336057ed0b08a7ed37bf64f081e380
Harry Liu <harryliu1995@gmail.com>
2024-09-27 12:44:08 +0200

    feat(aliases): Update aliases.

---
+++ setup/dotfiles/common/aliases/os/process_aliases.sh
@@ -0,0 +1,16 @@
+killport() {
+  if [ -z "$1" ]; then
+    echo "Usage: killport <port>"
+    return 1
+  fi
+
+  local port="$1"
+  local pid=$(lsof -t -i :$port)
+
+  if [ -z "$pid" ]; then
+    echo "No process found on port $port"
+  else
+    echo "Killing process $pid running on port $port"
+    kill -9 "$pid"
+  fi
+}
--- setup/dotfiles/zsh/aliases/devops_aliases.sh
+++ setup/dotfiles/zsh/aliases/devops_aliases.sh
@@ -1,26 +1,3 @@
-# Open Mail
 alias openmail="code /var/mail/$(whoami)"
 alias clearmail="echo '' > /var/mail/$(whoami)"
-
-# Port
-killport() {
-  if [ -z "$1" ]; then
-    echo "Usage: killport <port>"
-    return 1
-  fi
-
-  local port="$1"
-  local pid=$(lsof -t -i :$port)
-
-  if [ -z "$pid" ]; then
-    echo "No process found on port $port"
-  else
-    echo "Killing process $pid running on port $port"
-    kill -9 "$pid"
-  fi
-}
-
-dir=~/shell-aliases/devops/
-find "$dir" -name "*.sh" | while read -r script; do
-  source "$script"
-done
+load_aliases ~/shell-aliases/devops/
