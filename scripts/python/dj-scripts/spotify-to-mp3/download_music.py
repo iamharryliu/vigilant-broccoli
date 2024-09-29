adade0fa57b98c974865a36efc033539951c1c54
Harry Liu <harryliu1995@gmail.com>
2024-09-30 01:46:29 +0200

    refactor(script): Refactor script.

--- scripts/python/dj-scripts/spotify-to-mp3/download_music.py
+++ scripts/python/dj-scripts/spotify-to-mp3/download_music.py
@@ -1,29 +1,21 @@
 import json
 import os
-from time import time
 from spotify_to_mp3_service import SpotifyToMp3Service
 
 CUSTOM_PLAYLIST_FILENAME = "custom_playlists.json"
 DEFAULT_PLAYLIST_FILENAME = "default_playlists.json"
 
 
-def main():
-    start_time = time()
-    print("Downloading music started.")
+def main() -> None:
     filename = (
         CUSTOM_PLAYLIST_FILENAME
         if os.path.exists(CUSTOM_PLAYLIST_FILENAME)
         else DEFAULT_PLAYLIST_FILENAME
     )
-    with open(filename, "r") as file:
+    with open(filename, "r", encoding="utf-8") as file:
         playlists = json.load(file)
     service = SpotifyToMp3Service()
     service.download_playlists(playlists)
-    end_time = time()
-    time_passed = end_time - start_time
-    minutes = int(time_passed // 60)
-    seconds = int(time_passed % 60)
-    print(f"Downloading music complete in {minutes}m{seconds}s.")
 
 
 if __name__ == "__main__":
