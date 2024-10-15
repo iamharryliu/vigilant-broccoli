5b8d16c7efeb61fab747302bab30e5627fa82e5c
Harry Liu <harryliu1995@gmail.com>
2024-10-15 13:36:40 +0200

    refactor(dj-scripts): Refactor.

--- setup/dotfiles/zsh/scripts/download_music.sh
+++ setup/dotfiles/zsh/scripts/download_music.sh
@@ -1,13 +1,2 @@
 #!/bin/bash
-
-IP_ADDRESS="8.8.8.8"
-PING_COUNT=1
-ping -c $PING_COUNT $IP_ADDRESS > /dev/null 2>&1
-
-if [ $? -eq 0 ]; then
-    # DEBUG LINE
-    # echo "Ping to $IP_ADDRESS successful. Executing $0"
-    cd $REPO_DIR/scripts/python/dj-scripts/spotify-to-mp3 && source venv/bin/activate && python download_music.py
-else
-    echo "Ping to $IP_ADDRESS failed. Aborting the script execution."
-fi
+cd $REPO_DIR/scripts/python/dj-scripts/spotify-to-mp3 && source venv/bin/activate && python download_music.py
