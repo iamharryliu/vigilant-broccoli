a20b719606a60f0d59d8773b9995b23a950dfcff
Harry Liu <harryliu1995@gmail.com>
2024-10-15 20:01:28 +0200

    feat: Update TODO_SITE list.

--- projects/nx-workspace/libs/@prettydamntired/todo-lib/src/index.ts
+++ projects/nx-workspace/libs/@prettydamntired/todo-lib/src/index.ts
@@ -5,6 +5,7 @@ export interface Todo {
 
 export const TODO_SITE = {
   EXPRESS: 'https://express-demo.fly.dev/',
+  DOTNET: 'https://dotnet-demo.fly.dev/',
   REACT: 'https://react-demo-dcx.pages.dev/',
   VUE: 'https://vue-demo-5gs.pages.dev/',
 };
