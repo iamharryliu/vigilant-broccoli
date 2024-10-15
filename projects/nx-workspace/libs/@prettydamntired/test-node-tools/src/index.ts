894a5709973c173e16c88b3710bf8932d582233c
Harry Liu <harryliu1995@gmail.com>
2024-10-15 10:33:51 +0200

    feat: Add getListFromFilepath and getObjectFromFilepath.

--- projects/nx-workspace/libs/@prettydamntired/test-node-tools/src/index.ts
+++ projects/nx-workspace/libs/@prettydamntired/test-node-tools/src/index.ts
@@ -10,3 +10,29 @@ export { EncryptionService } from './services/encryption/encryption.service';
 export { RecaptchaService } from './services/recaptcha/recaptcha.service';
 export { SiteMonitor } from './services/site-monitor/site-monitor.service';
 export { TextMessageService } from './services/text-message/text-message.service';
+
+import fs from 'fs';
+
+export interface HashmapCache<T> {
+  [key: string]: T;
+}
+
+const getFromFilepath = <T>(filepath: string, structure: T): T => {
+  try {
+    const cache = JSON.parse(fs.readFileSync(filepath, { encoding: 'utf-8' }));
+    return cache as T;
+  } catch {
+    console.debug(`Filepath '${filepath}'not found.`);
+    return structure;
+  }
+};
+
+export const getListFromFilepath = <T>(filepath: string): T[] => {
+  return getFromFilepath(filepath, [] as T[]);
+};
+
+export const getHashmapFromFilepath = <T>(
+  filepath: string,
+): HashmapCache<T> => {
+  return getFromFilepath(filepath, {} as HashmapCache<T>);
+};
