aa7d780e9db62535d3d78d21699c8efb46feb866
Harry Liu <harryliu1995@gmail.com>
2025-01-26 23:21:58 +0100

    chore(nx): delete new-tab-dashboard

--- projects/nx-workspace/apps/ui/new-tab-dashboard/src/app/app.test.tsx
+++
@@ -1,7 +0,0 @@
-import { App } from './app';
-
-describe('App', () => {
-  test('index', async () => {
-    expect(App).toBeTruthy();
-  });
-});
--- projects/nx-workspace/apps/ui/new-tab-dashboard/src/app/app.tsx
+++
@@ -1,8 +0,0 @@
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
-import styles from './app.module.css';
-
-export function App() {
-  return <>New Tab Dashboard</>;
-}
-
-export default App;
