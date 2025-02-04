bf1c867f5fcb7347c984c77533e783f26b078218
Harry Liu <harryliu1995@gmail.com>
2025-02-04 03:03:52 +0100

    fix: rename env

--- projects/nx-workspace/libs/@vigilant-broccoli/common-js/src/index.ts
+++ projects/nx-workspace/libs/@vigilant-broccoli/common-js/src/index.ts
@@ -1,3 +1,3 @@
-export * from './lib/env/env.consts';
+export * from './lib/node-environment/node-environment.consts';
 export * from './lib/http/http.consts';
 export * from './lib/location/location.model';
