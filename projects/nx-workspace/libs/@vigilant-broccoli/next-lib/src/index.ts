915161d8d05629f48b0147a4917a2391cfe7ee3f
Harry Liu <harryliu1995@gmail.com>
2025-06-08 09:19:15 +0200

    chore: rm

--- projects/nx-workspace/libs/@vigilant-broccoli/next-lib/src/index.ts
+++ projects/nx-workspace/libs/@vigilant-broccoli/next-lib/src/index.ts
@@ -1,4 +1,1 @@
-// Use this file to export React client components (e.g. those with 'use client' directive) or other non-server utilities
-
-export * from './lib/next-lib';
 export * from './lib/NextNavBar';
