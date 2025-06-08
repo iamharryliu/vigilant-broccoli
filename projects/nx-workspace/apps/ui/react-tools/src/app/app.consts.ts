f0f7350baf1c8a0c120eec8dfee4ed0c2230f698
Harry Liu <harryliu1995@gmail.com>
2025-06-09 00:42:07 +0200

    chore: code cleanup

--- projects/nx-workspace/apps/ui/react-tools/src/app/app.consts.ts
+++ projects/nx-workspace/apps/ui/react-tools/src/app/app.consts.ts
@@ -1,33 +1,9 @@
-import { TextPage } from './components/pages/TextPage';
 import { IndexPage } from './components/pages/IndexPage';
-import { JSONPage } from './components/pages/JSONPage';
-import { ComponentDemoPage } from './components/pages/ComponentDemoPage';
-import { AIDemoPage } from './components/pages/AIDemoPage';
 
 export const APP_ROUTE = {
   INDEX: {
     title: 'Home',
     path: '/',
     component: IndexPage,
-  },
-  JSON: {
-    title: 'JSON',
-    path: '/json-tools',
-    component: JSONPage,
-  },
-  TEXT: {
-    title: 'Text Tools',
-    path: '/text-tools',
-    component: TextPage,
-  },
-  COMPONENT_LIBRARY: {
-    title: 'Component Library',
-    path: '/component-library',
-    component: ComponentDemoPage,
-  },
-  AI_TOOL: {
-    title: 'AI Tool',
-    path: '/ai-tool',
-    component: AIDemoPage,
   },
 };
