54bcd9c4ebeca22584302080123e3ff5e7d3ea43
Harry Liu <harryliu1995@gmail.com>
2025-06-09 09:13:39 +0200

    feat: add endpoint

---
+++ projects/nx-workspace/apps/next-demo/src/app/api/route.ts
@@ -0,0 +1,28 @@
+import { LLMModel, LLMPrompt } from '@vigilant-broccoli/common-js';
+import { NextRequest, NextResponse } from 'next/server';
+import { LLMService } from '@vigilant-broccoli/ai-tools';
+
+type Prompt = {
+  id: number;
+  prompt: LLMPrompt;
+  model: LLMModel;
+  results: string[];
+};
+
+export async function POST(request: NextRequest) {
+  const prompts = (await request.json()) as Prompt[];
+  const results = await Promise.all(
+    prompts.map(prompt =>
+      LLMService.prompt<string>({
+        prompt: prompt.prompt,
+        modelConfig: { model: prompt.model },
+      }),
+    ),
+  );
+  console.log(results);
+  return NextResponse.json(
+    prompts.map((prompt, i) => {
+      return { ...prompt, results: [...prompt.results, results[i].data] };
+    }),
+  );
+}
