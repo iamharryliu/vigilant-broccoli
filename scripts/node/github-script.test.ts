82519d28cb7f289d480337a9f4542751a7242d19
Harry Liu <harryliu1995@gmail.com>
2025-06-24 01:31:06 +0200

    test(github-iac): add tests

--- scripts/node/github-script.test.ts
+++ scripts/node/github-script.test.ts
@@ -5,31 +5,43 @@ import {
   afterEach,
   it,
   jest,
+  afterAll,
 } from '@jest/globals';
-import { FileSystemUtils } from '@vigilant-broccoli/common-node';
 import { GithubService } from './github.service';
 import { GithubOrganizationTeamStructure } from './github.types';
 import { configureGithubTeams } from './script';
+import {
+  DEFAULT_MOCK,
+  DEFAULT_MOCK_LESS_1_TEAM,
+  DEFAULT_MOCK_NO_TEAMS,
+} from './mock.consts';
 
-jest.setTimeout(30000);
+jest.setTimeout(90000);
 
 describe('GitHub Team Configuration', () => {
   const ORGANIZATION_NAME = 'gh-managment-test-org';
   let organizationData: GithubOrganizationTeamStructure;
 
-  beforeAll(async () => {
-    organizationData = FileSystemUtils.getObjectFromFilepath(
-      './test-teams.json',
-    ) as GithubOrganizationTeamStructure;
-  });
-
-  afterEach(async () => {
+  afterAll(async () => {
     await GithubService.deleteAllTeams(ORGANIZATION_NAME);
   });
 
   it('should configure GitHub teams to match the provided structure', async () => {
+    let organizationData = DEFAULT_MOCK as GithubOrganizationTeamStructure;
     await configureGithubTeams(organizationData);
-    const structure = await GithubService.getOrgStructure(ORGANIZATION_NAME);
+    let structure = await GithubService.getOrgStructure(ORGANIZATION_NAME);
     expect(structure).toEqual(organizationData);
+
+    organizationData =
+      DEFAULT_MOCK_LESS_1_TEAM as GithubOrganizationTeamStructure;
+    await configureGithubTeams(organizationData);
+    structure = await GithubService.getOrgStructure(ORGANIZATION_NAME);
+    expect(structure).toEqual(organizationData);
+
+    // TODO: implement flags for config types
+    // organizationData = DEFAULT_MOCK_NO_TEAMS as GithubOrganizationTeamStructure
+    // await configureGithubTeams(organizationData);
+    //  structure = await GithubService.getOrgStructure(ORGANIZATION_NAME);
+    // expect(structure).toEqual(organizationData);
   });
 });
