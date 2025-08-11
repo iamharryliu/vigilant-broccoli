a0e4285c825fdf84ee23d488ec74b3d5bd871f03
Harry Liu <harryliu1995@gmail.com>
2025-08-11 23:18:29 +0200

    feat(socket-demo): update socket server demo

--- projects/demo-apps/socket-demo/README.md
+++ projects/demo-apps/socket-demo/README.md
@@ -6,3 +6,22 @@ npx tsx
 # Connect to socket in terminal and type messages
 nc localhost 3000
 ```
+
+```
+docker build -t websocket-server .
+docker run -p 5000:5000 websocket-server
+```
+
+```
+[Unit]
+Description=Socket Service
+
+[Service]
+ExecStart=npx tsx server.ts
+WorkingDirectory=/opt/booshan-socket-listener
+Restart=always
+User=root
+
+[Install]
+WantedBy=multi-user.target
+```
--- projects/demo-apps/socket-demo/server.tsx
+++ projects/demo-apps/socket-demo/server.tsx
@@ -1,24 +1,73 @@
-const net = require('net');
+import { WebSocket, WebSocketServer } from 'ws';
+import { parse } from 'url';
+import 'dotenv/config';
 
-const PORT = 3000;
+const HOST = process.env.SOCKET_SERVER_HOST || '127.0.0.1';
+const PORT = Number(process.env.SOCKET_SERVER_PORT || 8080);
 
-const server = net.createServer(socket => {
-  console.log('Client connected');
+const wss = new WebSocketServer({ port: PORT, host: HOST });
 
-  socket.on('data', data => {
-    console.log(`Received: ${data}`);
-    socket.write(`Echo: ${data}`);
+const clients = new Map<string, Set<WebSocket>>();
+
+wss.on('connection', (ws, req) => {
+  console.log('New client connected');
+
+  const query = parse(req.url || '', true).query;
+  const userId = query.userId as string;
+  if (userId) {
+    console.log(`User connected: ${userId}`);
+    if (!clients.has(userId)) {
+      clients.set(userId, new Set());
+    }
+    clients.get(userId)?.add(ws);
+  }
+
+  ws.on('message', message => {
+    try {
+      const data = JSON.parse(message.toString('utf-8'));
+      sendNotification(data.receivers, JSON.stringify(data));
+    } catch (err) {
+      console.error('Error parsing message:', err);
+    }
   });
 
-  socket.on('end', () => {
-    console.log('Client disconnected');
-  });
-
-  socket.on('error', err => {
-    console.error(`Socket error: ${err.message}`);
+  ws.on('close', () => {
+    if (userId) {
+      console.log(`Client disconnected: ${userId}`);
+      const userSockets = clients.get(userId);
+      if (userSockets) {
+        userSockets.delete(ws);
+        if (userSockets.size === 0) {
+          clients.delete(userId);
+        }
+      }
+    }
   });
 });
 
-server.listen(PORT, () => {
-  console.log(`Server listening on port ${PORT}`);
-});
+console.log(`WebSocket server running on ws://${HOST}:${PORT}`);
+
+function sendToSockets(sockets: Set<WebSocket>, user: string, message: string) {
+  sockets.forEach(ws => {
+    if (ws.readyState === WebSocket.OPEN) {
+      console.log(`Sending message to ${user}`);
+      ws.send(message);
+    }
+  });
+}
+
+function sendNotification(receivers: string | string[], message: string) {
+  if (receivers === 'EVERYONE') {
+    for (const [user, sockets] of clients.entries()) {
+      sendToSockets(sockets, user, message);
+    }
+    return;
+  } else {
+    for (const receiver of receivers) {
+      const userSockets = clients.get(receiver);
+      if (userSockets) {
+        sendToSockets(userSockets, receiver, message);
+      }
+    }
+  }
+}
