f4f663c75a5d83b18393b29352dc58132220b5bd
Harry Liu <harryliu1995@gmail.com>
2025-11-01 10:48:23 +0100

    refactor: Refactor.

--- projects/nx-workspace/apps/ui/small-business-next/lib/auth.consts.ts
+++ projects/nx-workspace/apps/ui/small-business-next/lib/auth.consts.ts
@@ -1,22 +1,3 @@
-import type { NextAuthOptions } from 'next-auth';
-import GoogleProvider from 'next-auth/providers/google';
-
-export const NEXT_AUTH_OPTIONS: NextAuthOptions = {
-  providers: [
-    GoogleProvider({
-      clientId: process.env.NEXTAUTH_GOOGLE_CLIENT_ID as string,
-      clientSecret: process.env.NEXTAUTH_GOOGLE_CLIENT_SECRET as string,
-      authorization: {
-        params: {
-          prompt: 'consent',
-          access_type: 'offline',
-          response_type: 'code',
-        },
-      },
-    }),
-  ],
-};
-
 export const NEXT_AUTH_STATUS = {
   LOADING: 'loading',
   UNAUTHENTICATED: 'unauthenticated',
--- projects/nx-workspace/apps/ui/small-business-next/src/app/api/auth/[...nextauth]/route.ts
+++ projects/nx-workspace/apps/ui/small-business-next/src/app/api/auth/[...nextauth]/route.ts
@@ -1,5 +1,22 @@
-import { NEXT_AUTH_OPTIONS } from '../../../../../lib/auth.consts';
-import NextAuth from 'next-auth';
+import { getEnvironmentVariable } from '@vigilant-broccoli/common-node';
+import NextAuth, { NextAuthOptions } from 'next-auth';
+import GoogleProvider from 'next-auth/providers/google';
+
+const NEXT_AUTH_OPTIONS: NextAuthOptions = {
+  providers: [
+    GoogleProvider({
+      clientId: getEnvironmentVariable('NEXTAUTH_GOOGLE_CLIENT_ID'),
+      clientSecret: getEnvironmentVariable('NEXTAUTH_GOOGLE_CLIENT_SECRET'),
+      authorization: {
+        params: {
+          prompt: 'consent',
+          access_type: 'offline',
+          response_type: 'code',
+        },
+      },
+    }),
+  ],
+};
 
 const handler = NextAuth(NEXT_AUTH_OPTIONS);
 
