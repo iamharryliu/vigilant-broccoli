83ddcaca7004708c6913ef77b16fc91d3b718626
Harry Liu <harryliu1995@gmail.com>
2025-11-11 15:24:12 +0100

    feat(dotfiles): Update dotfiles.

--- setup/dotfiles/zsh/aliases/devops_aliases.sh
+++ setup/dotfiles/zsh/aliases/devops_aliases.sh
@@ -9,4 +9,12 @@ alias tfplan="terraform plan"
 alias tfapply="terraform apply"
 alias tfdestroy="terraform destroy"
 
+alias tfapplydev="terraform apply -var-file=dev.tfvars"
+alias tfdestroydev="terraform destroy -var-file=dev.tfvars"
+
+alias tfapplyprod="terraform apply -var-file=prod.tfvars"
+alias tfdestroyprod="terraform destroy -var-file=prod.tfvars"
+
+alias tfoutput="terraform output"
+
 alias openlocalvault="open 'https://127.0.0.1:8200'"
