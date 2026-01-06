bd181e6cfc2a03f643205982b735d0ae0cf61238
Harry Liu <harryliu1995@gmail.com>
2026-01-06 10:51:13 +0100

    ci: Update Terraform.

--- terraform/main.tf
+++ terraform/main.tf
@@ -29,6 +29,10 @@ resource "google_compute_instance" "vb_free_vm" {
   machine_type = "e2-micro" # Free tier eligible
   zone         = "us-east1-b"
 
+  metadata = {
+    ssh-keys = "harryliu:ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAACAQCdsUNm7YC0dLJ5zNDCNsLcT1c/ff6C/1i0IhAbqKXNGfT5ObXmTtnBOuH1UnTnRGIAr52agN6l5VMx62r90OsCh0+zbvTDSl7dWPvvXOwCXKrftuSKTH84r6gYsRiDGuh6j3zpfokCg1yJlcryp2Dgs1ua26DZm301NXkEaB9MSWYZzgeFv9fmWTgvCXpIHsRnSKV8PINDA3Ouavz1T6uqbAeNL71NVBwEqHlPDWtzGryQdbIS6tA2ufKB8KZSHCZjuORwm8K8Jaf6FIywMOx/3rKOl6u85pI7//D1TORP/pnt1Hn9Wd/QCwtL+J4nhv4eqHtarRpJXtyK7e1c/7Ga8FU/BjNodzA+Sfm5yDg+LZfBcxBVh+8KTCNc1QrmcmhVoPKEh9luG/v/5A8DpuzaG6Wr/c2YKVCU4MU+POFAP94D2M+MgvbHdMDj57oTtqHrNnY97A06LuLpouqmZ6ZG8imbV3pj+UpogpDvJCxZ+iF0+8LJaNnkRzZ84G+hBl5xgQ+JhQ5V7uLYUi0w8D+SwFTTxflTeDBmVgcqPB5TY3tvOasbFIDIP4xCLlPq1J0EaTaESNtfCC7nZPXPjjfjPzKrE56Ne0OLQQdvn1OALvYgSFl58rkSYj1M0esAbQYxfhMzpHovr49wShGg8F+ipCqMRRtzwDbN6fd84M5nTw== harryliu@Harrys-MacBook-Pro.local"
+  }
+
   # Boot disk configuration
   boot_disk {
     auto_delete = false # Keep disk when instance is deleted
@@ -73,7 +77,37 @@ resource "google_compute_instance" "vb_free_vm" {
 
   # Deletion protection disabled
   deletion_protection = false
-
-  # Allow stopping for updates
-  allow_stopping_for_update = true
 }
+
+resource "google_compute_firewall" "wireguard_ssh" {
+  name    = "allow-wireguard-access-ssh"
+  network = "default"
+
+  direction = "INGRESS"
+  priority  = 1000
+
+  allow {
+    protocol = "tcp"
+    ports    = ["22"]
+  }
+
+  source_ranges = ["10.0.1.0/24"]
+  target_tags   = ["wireguard"]
+}
+
+resource "google_compute_firewall" "wireguard" {
+  name    = "allow-wireguard-access"
+  network = "default"
+
+  direction = "INGRESS"
+  priority  = 1000
+
+  allow {
+    protocol = "udp"
+    ports    = ["51820"]
+  }
+
+  source_ranges = ["0.0.0.0/0"]
+  target_tags   = ["wireguard"]
+}
+
