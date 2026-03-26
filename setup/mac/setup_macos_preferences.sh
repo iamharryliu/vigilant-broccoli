7cf47c5ad85c42373767641bc04f71e2dfffb709
Harry Liu <harryliu1995@gmail.com>
2026-03-26 21:43:50 +0100

    feat: Prefer Finder list view.

--- setup/mac/setup_macos_preferences.sh
+++ setup/mac/setup_macos_preferences.sh
@@ -12,3 +12,8 @@ defaults write com.apple.dock wvous-bl-corner -int 13
 defaults write com.apple.dock wvous-bl-modifier -int 0
 defaults write com.apple.dock wvous-br-corner -int 4
 defaults write com.apple.dock wvous-br-modifier -int 0
+
+# Finder
+defaults write com.apple.Finder FXPreferredViewStyle Nlsv
+find / -name ".DS_Store" -delete 2>/dev/null
+killall Finder
