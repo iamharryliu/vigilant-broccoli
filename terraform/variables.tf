e35b2dd6bfe2f154eb6d7c752448e9148d0ac0be
Harry Liu <harryliu1995@gmail.com>
2026-04-18 19:22:06 +0200

    infrastructure: Implement oracle infrastructure. (#15)
    * infrastructure: Implement oracle infrastructure.
    
    * infrastructure: Enhance Oracle infrastructure.
    
    * infrastructure: Enhance Oracle infrastructure.

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -28,3 +28,36 @@ variable "cloudflare_api_token" {
   type      = string
   sensitive = true
 }
+
+variable "tenancy_ocid" {
+  type = string
+}
+
+variable "user_ocid" {
+  type = string
+}
+
+variable "fingerprint" {
+  type = string
+}
+
+variable "private_key_path" {
+  type    = string
+  default = "/Users/harryliu/.ssh/oci_api_key.pem"
+}
+
+variable "ssh_public_key" {
+  type    = string
+  default = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIISE8yIDUuc3MRphJDr212uIjQEqU+JFgwQCSL6VvHRw harryliu@Harrys-MacBook-Pro.local"
+}
+
+variable "rabbitmq_user" {
+  type    = string
+  default = "admin"
+}
+
+variable "rabbitmq_password" {
+  type      = string
+  default   = "changeme"
+  sensitive = true
+}
