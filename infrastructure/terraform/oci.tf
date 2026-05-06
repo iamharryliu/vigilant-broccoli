aa77ae39740ae6dcc2783c93be0723f6d895d0ff
Harry Liu <harryliu1995@gmail.com>
2026-05-06 18:54:28 +0200

    infrastructure: Help script, remove test script, update oci.tf.

--- infrastructure/terraform/oci.tf
+++ infrastructure/terraform/oci.tf
@@ -97,8 +97,9 @@ resource "oci_core_instance" "rabbitmq" {
   }
 
   source_details {
-    source_type = "image"
-    source_id   = data.oci_core_images.ubuntu_arm.images[0].id
+    source_type             = "image"
+    source_id               = data.oci_core_images.ubuntu_arm.images[0].id
+    boot_volume_size_in_gbs = 50
   }
 
   create_vnic_details {
