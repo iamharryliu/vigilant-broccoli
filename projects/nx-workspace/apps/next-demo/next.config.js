d6948f1f3fe84c5bb703358789b71a32b1d62c55
Harry Liu <harryliu1995@gmail.com>
2026-05-20 20:12:45 +0200

    fix: Vercel deploy fix attempt.

--- projects/nx-workspace/apps/next-demo/next.config.js
+++ projects/nx-workspace/apps/next-demo/next.config.js
@@ -7,9 +7,8 @@ const { composePlugins, withNx } = require('@nx/next');
  * @type {import('@nx/next/plugins/with-nx').WithNxOptions}
  **/
 const nextConfig = {
-  // Use this to set Nx-specific options
-  // See: https://nx.dev/recipes/next/next-config-setup
   nx: {},
+  serverExternalPackages: ['sharp'],
   transpilePackages: [
     '@fullcalendar/core',
     '@fullcalendar/react',
