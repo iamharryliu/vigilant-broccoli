c4bf4c31aee8f8a5654e679434393b711ef992c5
Harry Liu <harryliu1995@gmail.com>
2026-06-08 00:04:09 +0200

    chore: Update tmux-vb

--- setup/dotfiles/common/scripts/tmux-vb.sh
+++ setup/dotfiles/common/scripts/tmux-vb.sh
@@ -8,58 +8,42 @@ tmux has-session -t "$SESSION" 2>/dev/null && tmux attach -t "$SESSION" && exit 
 tmux new-session -d -s "$SESSION"
 
 #################################
-# Window 1: tools (4 panes)
+# Window 1: yazi
 #################################
-tmux rename-window -t "$SESSION:1" tools
-
-# Split into 2x2 grid
-tmux split-window -h  -t "$SESSION:1"
-tmux split-window -v  -t "$SESSION:1.1"
-tmux split-window -v  -t "$SESSION:1.2"
-
-tmux select-layout -t "$SESSION:1" tiled
-
-# Pane commands
+tmux rename-window -t "$SESSION:1" yazi
 tmux send-keys -t "$SESSION:1.1" "yazi" C-m
-tmux send-keys -t "$SESSION:1.2" "btop" C-m
-tmux send-keys -t "$SESSION:1.3" "lazydocker" C-m
-tmux send-keys -t "$SESSION:1.4" "posting" C-m
 
 #################################
-# Window 2: dev (4 panes)
+# Window 2: btop
+#################################
+tmux new-window -t "$SESSION" -n btop
+tmux send-keys -t "$SESSION:2.1" "btop" C-m
+
+#################################
+# Window 3: dev (4 panes)
 #################################
 tmux new-window -t "$SESSION" -n dev -c "$PROJECT"
 
 DEV_PANE=$(tmux display-message -t "$SESSION:dev" -p '#{pane_id}')
-
 tmux split-window -h -t "$DEV_PANE" -c "$PROJECT"
-DEV_RIGHT_LEFT=$(tmux display-message -t "$SESSION:dev" -p '#{pane_id}')
-
-tmux split-window -h -t "$DEV_RIGHT_LEFT" -c "$PROJECT"
-DEV_RIGHT_RIGHT_TOP=$(tmux display-message -t "$SESSION:dev" -p '#{pane_id}')
-
-tmux split-window -v -t "$DEV_RIGHT_RIGHT_TOP" -c "$PROJECT"
-DEV_RIGHT_RIGHT_BOTTOM=$(tmux display-message -t "$SESSION:dev" -p '#{pane_id}')
 
 tmux send-keys -t "$DEV_PANE" "cd $PROJECT && claude" C-m
-tmux send-keys -t "$DEV_RIGHT_RIGHT_BOTTOM" "cd $PROJECT && lazygit" C-m
-
 tmux select-pane -t "$DEV_PANE"
 
 #################################
-# Window 3: other
+# Window 4: other
 #################################
 tmux new-window -t "$SESSION" -n other
-tmux send-keys -t "$SESSION:3.1" "" C-m
+tmux send-keys -t "$SESSION:4.1" "" C-m
 
 #################################
-# Window 4: neovide
+# Window 5: neovide
 #################################
 tmux new-window -t "$SESSION" -n neovide
-tmux send-keys -t "$SESSION:4.1" "neovidetmuxvb" C-m
+tmux send-keys -t "$SESSION:5.1" "neovidetmuxvb" C-m
 
 #################################
 # Focus
 #################################
-tmux select-window -t "$SESSION:2"
+tmux select-window -t "$SESSION:3"
 tmux attach -t "$SESSION"
