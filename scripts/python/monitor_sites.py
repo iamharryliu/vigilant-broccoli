72fd0353e77ab3fd02c88e87c8bd6a0fb282a340
Harry Liu <harryliu1995@gmail.com>
2026-06-07 21:38:33 +0200

    chore: code cleanup

--- scripts/python/monitor_sites.py
+++
@@ -1,8 +0,0 @@
-from site_monitor import SiteMonitor
-import json
-
-with open("/etc/sites.json") as f:
-    servers = json.load(f)
-
-if __name__ == "__main__":
-    SiteMonitor.check_servers(servers)
