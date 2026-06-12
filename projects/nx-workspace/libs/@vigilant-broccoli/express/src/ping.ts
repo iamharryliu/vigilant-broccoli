d74e87fa07614821b7f6347cd9dd5fb605ab6373
Harry Liu <harryliu1995@gmail.com>
2026-06-12 15:46:28 +0200

    fix: fix build

--- projects/nx-workspace/libs/@vigilant-broccoli/express/src/ping.ts
+++ projects/nx-workspace/libs/@vigilant-broccoli/express/src/ping.ts
@@ -2,7 +2,7 @@ import { Router } from 'express';
 
 export const PING_PATH = '/__ping';
 
-export const pingRouter = Router();
+export const pingRouter: Router = Router();
 
 pingRouter.get(PING_PATH, (_req, res) => {
   res.json({ ok: true });
