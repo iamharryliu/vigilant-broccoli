425cdfa71fe7e6309953bd807e9d1342ba9b4430
Harry Liu <harryliu1995@gmail.com>
2026-07-08 08:07:44 +0200

    chore: format

--- scripts/python/dj-scripts/spotify-to-mp3/get_playlists.py
+++ scripts/python/dj-scripts/spotify-to-mp3/get_playlists.py
@@ -3,9 +3,7 @@ import json
 from download_music import get_spotify_playlists
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(
-        description="Get Spotify playlists as JSON"
-    )
+    parser = argparse.ArgumentParser(description="Get Spotify playlists as JSON")
     parser.add_argument(
         "--filter",
         "-f",
